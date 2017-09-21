f96666f43ea1df8e1f9e520650e2a27c7bd58308
Chen Xie <is.chenx@gmail.com>
2017-09-21 16:42:48 -0400

    add profile model, and register models

--- physionet-django/user/models.py
+++ physionet-django/user/models.py
@@ -25,6 +25,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+
 class User(AbstractBaseUser):
     """
     The user authentication model
@@ -32,6 +33,7 @@ class User(AbstractBaseUser):
     email = models.EmailField(max_length=255, unique=True)
     join_date = models.DateField(auto_now_add=True)
     last_login = models.DateField(null=True, blank=True)
+    profile = models.OneToOneField('user.Profile', related_name='User', blank=True, null=True)
 
     # Mandatory fields for the default authentication backend
     is_active = models.BooleanField(default=True)
@@ -40,7 +42,7 @@ class User(AbstractBaseUser):
     EMAIL_FIELD = 'email'
 
     # For createsuperuser. Don't include USERNAME_FIELD or password
-    #REQUIRED_FIELDS = ['username']
+    # REQUIRED_FIELDS = ['username']
 
     # Mandatory methods for default authentication backend
     def get_full_name(self):
@@ -58,11 +60,33 @@ class User(AbstractBaseUser):
     def has_perm(self, perm, obj=None):
         "Does the user have a specific permission?"
         return True
+
     def has_module_perms(self, app_label):
         "Does the user have permissions to view the app `app_label`?"
         return True
+
     @property
     def is_staff(self):
         "Is the user a member of staff?"
         return self.is_admin
 
+
+class Profile(models.Model):
+    """
+    Class storing profile information which is
+    not directly related to account activity
+    or authentication.
+
+    This model should contain some fields which help map
+    projects to datacite:
+    https://schema.datacite.org/
+    https://schema.datacite.org/meta/kernel-4.0/doc/DataCite-MetadataKernel_v4.0.pdf
+    """
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=100)
+
+    organization = models.CharField(max_length=100, default='',)
+    url = models.URLField(default='', blank=True, null=True)
+
+    def __str__(self):
+        return ' '.join([self.first_name, self.last_name])
