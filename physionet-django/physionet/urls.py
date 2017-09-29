2a25eaec160a855eeada06426601814e3b1be105
Chen Xie <is.chenx@gmail.com>
2017-09-29 17:51:09 -0400

    add templates, js, css

--- physionet-django/physionet/urls.py
+++ physionet-django/physionet/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from .views import home
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url(r'^$', home),
 ]
