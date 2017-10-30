6b899afdc51807e66b75ad933b6a827933e194f9
Tom Pollard <tpollard@mit.edu>
2017-10-30 18:27:38 -0400

    tweak wording on form.

--- physionet-django/user/forms.py
+++ physionet-django/user/forms.py
@@ -12,7 +12,8 @@ class EmailForm(forms.Form):
     email = forms.EmailField(
         label='Email',
         max_length=254,
-        widget=forms.TextInput(attrs={'autofocus': True, 'class':'form-control', 'placeholder':'Email Address'}),
+        widget=forms.TextInput(attrs={'autofocus': True, 'class':'form-control', 
+            'placeholder':'Email Address'}),
     )
 
 class UserCreationForm(forms.ModelForm):
@@ -46,7 +47,8 @@ class UserCreationForm(forms.ModelForm):
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError("The passwords don't match")
         self.instance.username = self.cleaned_data.get('username')
-        password_validation.validate_password(self.cleaned_data.get('password2'), self.instance)
+        password_validation.validate_password(self.cleaned_data.get('password2'), 
+            self.instance)
         return password2
 
     def save(self, commit=True):
@@ -90,7 +92,8 @@ class LoginForm(auth_forms.AuthenticationForm):
     username = auth_forms.UsernameField(
         label='Email',
         max_length=254,
-        widget=forms.TextInput(attrs={'autofocus': True, 'class':'form-control', 'placeholder':'Email Address'}),
+        widget=forms.TextInput(attrs={'autofocus': True, 'class':'form-control', 
+            'placeholder':'Email Address'}),
     )
     password = forms.CharField(
         label= 'Password',
