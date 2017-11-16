9dd68af59732a6660616cccd43a32cdb9b5cc6f8
Chen Xie <is.chenx@gmail.com>
2017-11-15 21:09:24 -0500

    add coverage to requirements. Comment out test view content

--- physionet-django/user/views.py
+++ physionet-django/user/views.py
@@ -238,18 +238,18 @@ def verify_email(request, uidb64, token):
         {'title':'Invalid Verification Link', 'isvalid':False})
 
 
-def test(request):
-    """
-    For testing
-    """
-    user = request.user
-    primary_email_form = AssociatedEmailChoiceForm(label='Primary Email')
-    primary_email_form.get_associated_emails(user=user)
+# def test(request):
+#     """
+#     For testing
+#     """
+#     user = request.user
+#     primary_email_form = AssociatedEmailChoiceForm(label='Primary Email')
+#     primary_email_form.get_associated_emails(user=user)
 
-    if request.method == 'POST':
-        form = AssociatedEmailChoiceForm(request.POST)
+#     if request.method == 'POST':
+#         form = AssociatedEmailChoiceForm(request.POST)
 
-        pdb.set_trace()
+#         pdb.set_trace()
 
-    return render(request,'user/test.html', {'user':user,
-        'form':primary_email_form, 'csrf_token': csrf.get_token(request)})
+#     return render(request,'user/test.html', {'user':user,
+#         'form':primary_email_form, 'csrf_token': csrf.get_token(request)})
