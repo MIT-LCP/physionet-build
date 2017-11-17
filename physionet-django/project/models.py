dc022d2e837fe8fa7d0ce487caabee3e0d825c6c
Chen Xie <is.chenx@gmail.com>
2017-11-17 17:52:34 -0500

    more model fields for projects

--- physionet-django/project/models.py
+++ physionet-django/project/models.py
@@ -15,49 +15,44 @@ class Project(models.Model):
     https://schema.datacite.org/
     https://schema.datacite.org/meta/kernel-4.0/doc/DataCite-MetadataKernel_v4.0.pdf
     """
-    # 0=pre-submission, 1=under review, 2=published and inactive,
-    # 3=revising post-submission
-    status = models.SmallIntegerField(default=0)
-
-    owner = models.ForeignKey('user.User', related_name='owned_projects')
-    collaborators = models.ManyToManyField('user.User', related_name='collaborating_projects')
-    editor = models.ForeignKey('user.User', related_name='editing_projects', null=True)
-    reviewers = models.ManyToManyField('user.User', related_name='reviewing_projects')
-
-    # Allowed storage capacity in GB
-    storage_allowance = models.SmallIntegerField(default=10)
-    creation_date = models.DateTimeField(auto_now_add=True)
-
-
+    # Content description
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=20, unique=True, null=True)
+    abstract = RichTextField(max_length=10000)
+    background = RichTextField()
+    methods = RichTextField()
+    acknowledgements = RichTextField()
+    paper_citations = models.ManyToManyField('project.Reference', related_name='%(class)s_citations')
+    references = models.ManyToManyField('project.Reference', related_name='%(class)s_references')
+    topics = models.ManyToManyField('project.Topic', related_name='%(class)s')
 
     # Access policy 
     dua = models.ForeignKey('project.DUA', null=True, blank=True, related_name='%(class)s')
     training_course = models.ForeignKey('project.TrainingCourse', null=True, blank=True, related_name='%(class)s')
     id_verification_required = models.BooleanField(default=False)
 
-    # Project metadata
-    topics = models.ManyToManyField('project.Topic', related_name='%(class)s')
+    # Back-end information    
+    creation_date = models.DateTimeField(auto_now_add=True)
+    # Allowed storage capacity in GB
+    storage_allowance = models.SmallIntegerField(default=10)
+    owner = models.ForeignKey('user.User', related_name='owned_projects')
+    collaborators = models.ManyToManyField('user.User', related_name='collaborating_projects')
+    editor = models.ForeignKey('user.User', related_name='editing_projects', null=True)
+    reviewers = models.ManyToManyField('user.User', related_name='reviewing_projects')
+    # 0=prepublish, 1=under review (unable to edit), 2=published static,
+    # Core projects cycles back and forth between 0-1 until editor agrees to
+    # publish.
+    status = models.SmallIntegerField(default=0)
 
-    # Project description
-    abstract = RichTextField(max_length=10000)
-    background = RichTextField()
-    methods = RichTextField()
-    technical_validation = RichTextField()
-    usage_notes = RichTextField()
-    acknowledgements = RichTextField()
-    paper_citations = models.ManyToManyField('project.Reference', related_name='%(class)s_citations')
-    references = models.ManyToManyField('project.Reference', related_name='%(class)s_references')
-
-    # ------- Information for published projects only ------- #
+    # Information for published projects
     core_project = models.ForeignKey('project.Project', related_name='releases', blank=True, null=True)
+    # Changes from previous version (if any)
+    changelog = models.RichTextField()
     # Total file storage size in bytes
     storage_size = models.IntegerField(null=True)
     release_date = models.DateField(null=True)
     version_number = models.FloatField(null=True)
     doi = models.CharField(max_length=50, default='')
-
 
     class Meta:
         abstract = True
@@ -70,19 +65,34 @@ class Project(models.Model):
 
 
 class Database(Project):
-
+    """
+    A database project
+    """
+    technical_validation = RichTextField()
     data_description = RichTextField()
+    usage_notes = RichTextField()
+
 
 class SoftwarePackage(Project):
-    pass
+    """
+    A software project
+    """
+    technical_validation = RichTextField()
+    usage_notes = RichTextField()
+
 
 class Tutorial(Project):
+    """
+    A tutorial project
+    """
     pass
+
 
 class Challenge(Project):
-    pass
-
-
+    """
+    A challenge project
+    """
+    submission_deadline = models.DateTimeField()
 
 
 class Review(models.Model):
@@ -90,6 +100,7 @@ class Review(models.Model):
     Project review
     """
     project = models.ForeignKey('project.Project', related_name='reviews')
+    submission_date = models.DateTimeField(null=True)
 
 
 class StorageRequest(models.Model):
