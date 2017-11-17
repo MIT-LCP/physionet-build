009f7ae920d6618a89b1d38ad08e45df3bced2d5
Chen Xie <is.chenx@gmail.com>
2017-11-17 17:08:45 -0500

    begin to redefine project classes

--- physionet-django/project/models.py
+++ physionet-django/project/models.py
@@ -6,44 +6,9 @@ from ckeditor.fields import RichTextField
 from user.models import BaseAffiliation
 
 
-class BaseProject(models.Model):
+class Project(models.Model):
     """
-    Base class for project models to inherit from
-    """
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=20, unique=True, null=True)
-
-    # Access policy 
-    dua = models.ForeignKey('project.DUA', null=True, blank=True, related_name='%(class)s')
-    training_course = models.ForeignKey('project.TrainingCourse', null=True, blank=True, related_name='%(class)s')
-    id_verification_required = models.BooleanField(default=False)
-
-    # Project metadata
-    doi = models.CharField(max_length=50, default='')
-    resource_type = models.ForeignKey('project.ResourceType', null=True, related_name='%(class)s')
-    topics = models.ManyToManyField('project.Topic', related_name='%(class)s')
-
-    # Project description
-    abstract = RichTextField(max_length=1000)
-    background = RichTextField()
-    methods = RichTextField()
-    data_description = RichTextField()
-    technical_validation = RichTextField()
-    usage_notes = RichTextField()
-    acknowledgements = RichTextField()
-    paper_citations = models.ManyToManyField('project.Reference', related_name='%(class)s_citations')
-    references = models.ManyToManyField('project.Reference', related_name='%(class)s_references')
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        abstract = True
-
-
-class Project(BaseProject):
-    """
-    The core project model
+    The core project model to be inherited by different project types
 
     This model should contain some fields which help map
     projects to datacite:
@@ -59,16 +24,82 @@ class Project(BaseProject):
     editor = models.ForeignKey('user.User', related_name='editing_projects', null=True)
     reviewers = models.ManyToManyField('user.User', related_name='reviewing_projects')
 
+    # Allowed storage capacity in GB
+    storage_allowance = models.SmallIntegerField(default=10)
+    creation_date = models.DateTimeField(auto_now_add=True)
 
-class PublishedProject(BaseProject):
-    """
-    A public release of a project
-    """
-    core_project = models.ForeignKey('project.Project', related_name='published_projects')
 
-    size = models.IntegerField()
-    release_date = models.DateField(auto_now_add=True)
-    version_number = models.FloatField(default=1.0)
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=20, unique=True, null=True)
+
+    # Access policy 
+    dua = models.ForeignKey('project.DUA', null=True, blank=True, related_name='%(class)s')
+    training_course = models.ForeignKey('project.TrainingCourse', null=True, blank=True, related_name='%(class)s')
+    id_verification_required = models.BooleanField(default=False)
+
+    # Project metadata
+    topics = models.ManyToManyField('project.Topic', related_name='%(class)s')
+
+    # Project description
+    abstract = RichTextField(max_length=10000)
+    background = RichTextField()
+    methods = RichTextField()
+    technical_validation = RichTextField()
+    usage_notes = RichTextField()
+    acknowledgements = RichTextField()
+    paper_citations = models.ManyToManyField('project.Reference', related_name='%(class)s_citations')
+    references = models.ManyToManyField('project.Reference', related_name='%(class)s_references')
+
+    # ------- Information for published projects only ------- #
+    core_project = models.ForeignKey('project.Project', related_name='releases', blank=True, null=True)
+    # Total file storage size in bytes
+    storage_size = models.IntegerField(null=True)
+    release_date = models.DateField(null=True)
+    version_number = models.FloatField(null=True)
+    doi = models.CharField(max_length=50, default='')
+
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+    def __unique__(self):
+        if self.title not in set([title for d.title in DataBase.objects.all()])
+
+
+class Database(Project):
+
+    data_description = RichTextField()
+
+class SoftwarePackage(Project):
+    pass
+
+class Tutorial(Project):
+    pass
+
+class Challenge(Project):
+    pass
+
+
+
+
+class Review(models.Model):
+    """
+    Project review
+    """
+    project = models.ForeignKey('project.Project', related_name='reviews')
+
+
+class StorageRequest(models.Model):
+    """
+    A request for storage capacity for a project
+    """
+    # Requested storage size in GB
+    storage_size = models.SmallIntegerField()
+    project = models.OneToOneField('project.Project')
+    request_date = models.DateTimeField(auto_now_add=True)
 
 
 class AuthorInfo(models.Model):
@@ -78,8 +109,8 @@ class AuthorInfo(models.Model):
     Static snapshot/manually entered info, rather than info
     being based on profile fields liable to change.
     """
-    # The project_object points to either a Project or a PublishedProject.
-    # Do not confuse this content_type variable with project resource type.
+    # The project_object points to one of the project models.
+    # Do not confuse this built in content_type variable with project resource type.
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     project_object = GenericForeignKey('content_type', 'object_id')
@@ -99,14 +130,6 @@ class AffiliationInfo(BaseAffiliation):
     Author affiliation snapshot upon project publication.
     """
     author_info = models.ForeignKey('project.AuthorInfo', related_name='affiliations')
-
-
-class ResourceType(models.Model):
-    """
-    The general resource class of the project
-    eg. data, software, tutorial
-    """
-    description = models.CharField(max_length=50)
 
 
 class Topic(models.Model):
