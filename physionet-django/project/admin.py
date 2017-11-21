081ff85c5c4f03ca0c99e5a72ded88ba9cb6f478
Chen Xie <is.chenx@gmail.com>
2017-11-21 15:24:47 -0500

    create/separate 'project' class

--- physionet-django/project/admin.py
+++ physionet-django/project/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Project
+from .models import Database, SoftwarePackage
 
-admin.site.register(Project)
+admin.site.register(Database)
+admin.site.register(SoftwarePackage)
