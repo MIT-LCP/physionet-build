aa58ef91f6b4e7c35f55a4bb8b94e654ee74d074
Chen Xie <is.chenx@gmail.com>
2017-11-22 16:45:12 -0500

    updating forms and saves for project model

--- physionet-django/project/views.py
+++ physionet-django/project/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .forms import ProjectCreationForm
 
+import pdb
+
 @login_required
 def create_project(request):
-    form = ProjectCreationForm()
+    user = request.user
+    form = ProjectCreationForm(initial={'owner':user})
+
+    if request.method == 'POST':
+        form = ProjectCreationForm(request.POST)
+        pdb.set_trace()
+        if form.is_valid():
+            print('\n\nvalid!')
+            project = form.save(owner=user)
+            redirect('home')
+
+            # Registration successful
+            return render(request, 'home', {'user':user})
+
     return render(request, 'project/create_project.html', {'form':form})
