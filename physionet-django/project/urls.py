9c1aac0ddd111c36210773557c98634f1de75079
Chen Xie <is.chenx@gmail.com>
2017-12-11 14:38:05 -0500

    get rid of title and slug for project model itself

--- physionet-django/project/urls.py
+++ physionet-django/project/urls.py
@@ -1,17 +1,15 @@
 from django.conf.urls import url
 from django.urls import reverse_lazy
 
-from .views import (project_home, create_project, edit_project,
-    project_collaborators, project_files, project_metadata)
-
+from . import views
 
 urlpatterns = [
-    url(r'^$', project_home, name='project_home'),
-    url(r'^create/$', create_project, name='create_project'),
+    url(r'^$', views.project_home, name='project_home'),
+    url(r'^create/$', views.create_project, name='create_project'),
 
-    url(r'^(?P<project>[0-9A-Za-z_\-]+)/$', edit_project, name='edit_project'),
+    url(r'^(?P<project_id>\d+)/$', views.edit_project, name='edit_project'),
 
-    url(r'^(?P<project>[0-9A-Za-z_\-]+)/collaborators/$', project_collaborators, name='project_collaborators'),
-    url(r'^(?P<project>[0-9A-Za-z_\-]+)/files/$', project_files, name='project_files'),
-    url(r'^(?P<project>[0-9A-Za-z_\-]+)/metadata/$', project_metadata, name='project_metadata'),
+    url(r'^(?P<project_id>\d+)/collaborators/$', views.project_collaborators, name='project_collaborators'),
+    url(r'^(?P<project_id>\d+)/files/$', views.project_files, name='project_files'),
+    url(r'^(?P<project_id>\d+)/metadata/$', views.project_metadata, name='project_metadata'),
 ]
