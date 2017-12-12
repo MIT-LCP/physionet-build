7cd6c5faa97bb56013d17aa0633c1dfb9557956e
Felipe Torres <felipe.torres.cs@gmail.com>
2017-12-12 12:50:24 -0500

    fixed the password progress bar bug of different ids

--- physionet-django/static/zxcvbn/zxcvbn_ProgressBar_Change.js
+++ physionet-django/static/zxcvbn/zxcvbn_ProgressBar_Change.js
@@ -1,7 +1,14 @@
 $(document).ready(function()
 {
+	var name = document.getElementById("helper").getAttribute("data-name");
+	var last = document.getElementById("helper").getAttribute("data-last");
+	var middle = document.getElementById("helper").getAttribute("data-middle");
+	var url = document.getElementById("helper").getAttribute("data-url");
+	var phone = document.getElementById("helper").getAttribute("data-phone");
 	$("#StrengthProgressBar").zxcvbnProgressBar({
-		passwordInput: "#id_password", 
-		userInputs: []
+		passwordInput: "#id_new_password1", 
+		userInputs: [name, last, middle, url, phone]
 	});
 });
+
+
--- physionet-django/static/zxcvbn/zxcvbn_ProgressBar_Register.js
+++ physionet-django/static/zxcvbn/zxcvbn_ProgressBar_Register.js
@@ -1,7 +1,7 @@
-$(document).ready(function()
+$("input").change(function()
 {
 	$("#StrengthProgressBar").zxcvbnProgressBar({
-		passwordInput: "#id_password", 
+		passwordInput: "#id_password1", 
 		userInputs: [$("#id_email").val(), $("#id_first_name").val(), 
 			$("#id_middle_names").val(), $("#id_last_name").val()]
 	});
