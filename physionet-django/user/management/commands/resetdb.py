b3b9abc3fe351d106eb72581703550360b468fe4
Chen Xie <is.chenx@gmail.com>
2018-04-18 15:57:49 -0400

    changed database management commands

--- physionet-django/user/management/commands/resetdb.py
+++ physionet-django/user/management/commands/resetdb.py
@@ -1,15 +1,18 @@
 """
 Command to:
-- delete the sqlite database
+- delete all data from tables
+- drop all tables
 - delete migrations
-- apply and make migrations
-- load example fixture data
+- make migrations
+- apply migrations
 
-This should only be accessible in development environments.
+Does NOT load any data. This should only be used in development environments.
+
+Reference: https://code.djangoproject.com/ticket/23833
 
 """
 import os
-import shutil
+import sys
 
 from django.conf import settings
 from django.core.management import call_command
@@ -19,47 +22,44 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if os.environ['DJANGO_SETTINGS_MODULE'] != 'physionet.settings.development':
-            raise Exception('This command should only be called in a development environment')
+        # If not in development, prompt warning messages twice
+        if 'development' not in os.environ['DJANGO_SETTINGS_MODULE']:
+            warning_messages = ['You are NOT in the development environment. Are you sure you want to reset the database? [y/n]',
+                                'All the data will be removed, and existing migration files will be deleted. Are you sure? [y/n]',
+                                'Final warning. Are you ABSOLUTELY SURE? [y/n]']
+            for i in range(3):
+                choice = input(warning_messages[i]).lower()
+                if choice != 'y':
+                    sys.exit('Exiting from reset. No actions applied.')
+            print('Continuing reset')
 
         project_apps = ['user', 'project']
-        clear_db(project_apps)
-        load_fixtures(project_apps)
+
+        # Remove data from all tables. Tables are kept.
+        call_command('flush', interactive=False, verbosity=1)
+
+        for app in project_apps:
+            # Reverse migrations, which drops the tables. Only works if
+            # migration files exist, regardless of table/migration status.
+            try:
+                call_command('migrate', app, 'zero', verbosity=1)
+            except:
+                pass
+            # Delete the migration .py files
+            remove_migration_files(app)
+
+        # Remake and apply the migrations
+        call_command('makemigrations')
+        call_command('migrate')
 
 
 def remove_migration_files(app):
-    '''Remove all python migration files from registered apps'''
+    """
+    Remove all python migration files from an app
+
+    """
     app_migrations_dir = os.path.join(settings.BASE_DIR, app, 'migrations')
     if os.path.isdir(app_migrations_dir):
         migration_files = [file for file in os.listdir(app_migrations_dir) if file != '__init__.py' and file.endswith('.py')]
         for file in migration_files:
             os.remove(os.path.join(app_migrations_dir, file))
-
-def clear_db(project_apps):
-    """
-    Delete the database and migration files.
-    Remake and reapply the migrations
-    """
-    for app in project_apps:
-        remove_migration_files(app)
-
-    # delete the sqlite database file
-    db_file = os.path.join(settings.BASE_DIR, 'db.sqlite3')
-    if os.path.isfile(db_file):
-        os.remove(db_file)
-
-    # Remake and reapply the migrations
-    call_command('makemigrations')
-    call_command('migrate')
-
-
-def load_fixtures(project_apps):
-    """
-    Insert the demo content from each app's fixtures files.
-
-    Demo Profile objects are located in a separate user_profiles.json fixture
-    file as they can only be attached after the triggered profiles created
-    are removed.
-    """
-    for app in project_apps:
-        call_command('loaddata', app, verbosity=1)
