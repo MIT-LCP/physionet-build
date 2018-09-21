f9dd17ec1bb932c1f4d5187deada36b1232557fa
Chen Xie <is.chenx@gmail.com>
2018-09-21 15:38:09 -0400

    rename and restructure console page for submissions

--- physionet-django/console/views.py
+++ physionet-django/console/views.py
@@ -26,104 +26,9 @@ def is_admin(user, *args, **kwargs):
 
 @login_required
 @user_passes_test(is_admin)
-def console_home(request):
+def active_submissions(request):
     """
-    List of submissions the editor is responsible for
-    """
-    submissions = Submission.objects.filter(is_active=True,
-        submission_status__in=[0], editor=request.user)
-
-    return render(request, 'console/console_home.html',
-        {'submissions':submissions})
-
-
-def process_storage_response(request, storage_response_formset):
-    """
-    Implement the response to a storage request.
-    Helper function to view: storage_requests.
-    """
-    storage_request_id = int(request.POST['storage_response'])
-
-    for storage_response_form in storage_response_formset:
-        # Only process the response that was submitted
-        if storage_response_form.instance.id == storage_request_id:
-            if storage_response_form.is_valid() and storage_response_form.instance.is_active:
-                storage_request = storage_response_form.instance
-                storage_request.responder = request.user
-                storage_request.response_datetime = timezone.now()
-                storage_request.is_active = False
-                storage_request.save()
-
-                project = storage_request.project
-
-                if storage_request.response:
-                    project.storage_allowance = storage_request.request_allowance
-                    project.save()
-
-                # Send the notifying email to the submitting author
-                response = RESPONSE_ACTIONS[storage_request.response]
-                subject = 'Storage request {0} for project {1}'.format(response,
-                    project.title)
-                email, name = project.get_submitting_author_info()
-                body = loader.render_to_string('console/email/storage_response_notify.html',
-                    {'name':name, 'project':project, 'response':response,
-                     'allowance':storage_request.request_allowance})
-                send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
-                          [email], fail_silently=False)
-                messages.success(request, 'The storage request has been {0}.'.format(response))
-
-
-@login_required
-@user_passes_test(is_admin)
-def project_list(request):
-    """
-    View list of projects
-    """
-    projects = Project.objects.all()
-
-    # title, submitting author, creation date, published,
-    return render(request, 'console/project_list.html', {'projects':projects})
-
-
-@login_required
-@user_passes_test(is_admin)
-def storage_requests(request):
-    """
-    Page for listing and responding to project storage requests
-    """
-    user = request.user
-
-    StorageResponseFormSet = modelformset_factory(StorageRequest,
-        fields=('response', 'response_message'),
-        widgets={'response':Select(choices=forms.RESPONSE_CHOICES),
-                 'response_message':Textarea()}, extra=0)
-
-    if request.method == 'POST':
-        storage_response_formset = StorageResponseFormSet(request.POST)
-        process_storage_response(request, storage_response_formset)
-
-    storage_response_formset = StorageResponseFormSet(
-        queryset=StorageRequest.objects.filter(is_active=True))
-
-    return render(request, 'console/storage_requests.html', {'user':user,
-        'storage_response_formset':storage_response_formset})
-
-
-@login_required
-@user_passes_test(is_admin)
-def user_list(request):
-    """
-    View list of users
-    """
-    users = User.objects.all()
-    return render(request, 'console/user_list.html', {'users':users})
-
-
-@login_required
-@user_passes_test(is_admin)
-def submissions(request):
-    """
-    Submission control panel. Editors are assigned here.
+    List of active submissions. Editors are assigned here.
     """
     if request.method == 'POST':
         assign_editor_form = forms.AssignEditorForm(request.POST)
@@ -146,9 +51,22 @@ def submissions(request):
     assign_editor_form = forms.AssignEditorForm()
     n_awaiting_editor = submissions.filter(submission_status=0, editor=None).count()
 
-    return render(request, 'console/submissions.html',
+    return render(request, 'console/active_submissions.html',
         {'submissions':submissions, 'assign_editor_form':assign_editor_form,
          'n_awaiting_editor':n_awaiting_editor})
+
+
+@login_required
+@user_passes_test(is_admin)
+def editing_submissions(request):
+    """
+    List of submissions the editor is responsible for
+    """
+    submissions = Submission.objects.filter(is_active=True,
+        submission_status__in=[0], editor=request.user)
+
+    return render(request, 'console/editing_submissions.html',
+        {'submissions':submissions})
 
 
 @login_required
@@ -213,3 +131,84 @@ def edit_submission(request, submission_id):
 
     return render(request, 'console/edit_submission.html',
         {'submission':submission, 'edit_submission_form':edit_submission_form})
+
+
+def process_storage_response(request, storage_response_formset):
+    """
+    Implement the response to a storage request.
+    Helper function to view: storage_requests.
+    """
+    storage_request_id = int(request.POST['storage_response'])
+
+    for storage_response_form in storage_response_formset:
+        # Only process the response that was submitted
+        if storage_response_form.instance.id == storage_request_id:
+            if storage_response_form.is_valid() and storage_response_form.instance.is_active:
+                storage_request = storage_response_form.instance
+                storage_request.responder = request.user
+                storage_request.response_datetime = timezone.now()
+                storage_request.is_active = False
+                storage_request.save()
+
+                project = storage_request.project
+
+                if storage_request.response:
+                    project.storage_allowance = storage_request.request_allowance
+                    project.save()
+
+                # Send the notifying email to the submitting author
+                response = RESPONSE_ACTIONS[storage_request.response]
+                subject = 'Storage request {0} for project {1}'.format(response,
+                    project.title)
+                email, name = project.get_submitting_author_info()
+                body = loader.render_to_string('console/email/storage_response_notify.html',
+                    {'name':name, 'project':project, 'response':response,
+                     'allowance':storage_request.request_allowance})
+                send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
+                          [email], fail_silently=False)
+                messages.success(request, 'The storage request has been {0}.'.format(response))
+
+@login_required
+@user_passes_test(is_admin)
+def storage_requests(request):
+    """
+    Page for listing and responding to project storage requests
+    """
+    user = request.user
+
+    StorageResponseFormSet = modelformset_factory(StorageRequest,
+        fields=('response', 'response_message'),
+        widgets={'response':Select(choices=forms.RESPONSE_CHOICES),
+                 'response_message':Textarea()}, extra=0)
+
+    if request.method == 'POST':
+        storage_response_formset = StorageResponseFormSet(request.POST)
+        process_storage_response(request, storage_response_formset)
+
+    storage_response_formset = StorageResponseFormSet(
+        queryset=StorageRequest.objects.filter(is_active=True))
+
+    return render(request, 'console/storage_requests.html', {'user':user,
+        'storage_response_formset':storage_response_formset})
+
+
+@login_required
+@user_passes_test(is_admin)
+def project_list(request):
+    """
+    View list of projects
+    """
+    projects = Project.objects.all()
+
+    # title, submitting author, creation date, published,
+    return render(request, 'console/project_list.html', {'projects':projects})
+
+
+@login_required
+@user_passes_test(is_admin)
+def user_list(request):
+    """
+    View list of users
+    """
+    users = User.objects.all()
+    return render(request, 'console/user_list.html', {'users':users})
