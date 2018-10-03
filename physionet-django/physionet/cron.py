48db8a8264a0f60918a71e2cf4ee5d55d4c364b6
Chen Xie <is.chenx@gmail.com>
2018-10-03 14:31:44 -0400

    update email wording and reduce queryset expired object

--- physionet-django/physionet/cron.py
+++ physionet-django/physionet/cron.py
@@ -1,61 +1,69 @@
 from django_cron import CronJobBase, Schedule
-
 from django.conf import settings
 from django.core.mail import send_mail
 from django.template import loader
 from django.utils import timezone
 
 from user.models import AssociatedEmail
-from project.models import Invitation
+from project.models import AuthorInvitation
 
-TIME_LIMIT = 15
+
+UNVERIFIED_DAY_LIMIT = 15
+
 
 class RemoveUnverifiedEmails(CronJobBase):
     RUN_EVERY_MINS = 1
     RETRY_AFTER_FAILURE_MINS = 5
 
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS, 
+    schedule = Schedule(run_every_mins=RUN_EVERY_MINS,
         retry_after_failure_mins=RETRY_AFTER_FAILURE_MINS)
 
     code = 'physionet.RemoveUnverifiedEmails'
 
     def do(self):
-        users = AssociatedEmail.objects.filter(is_primary_email=False, is_verified=False,
-            added_date__lt=timezone.now() - timezone.timedelta(days=TIME_LIMIT))
-        for person in users:
-            AssociatedEmail.objects.get(id=person.id).delete()
-            print('{0}: Deleted email {1} from user {2}'.format(
-                timezone.now().strftime('%Y-%m-%d %H:%M:%S'), person.email, 
-                person.user.email))
+        associated_emails = AssociatedEmail.objects.filter(
+            is_primary_email=False, is_verified=False,
+            added_date__lt=timezone.now() - timezone.timedelta(
+                days=UNVERIFIED_DAY_LIMIT))
+
+        for ae in associated_emails:
+            ae.delete()
+            print('{}: Deleted email {} from user {}'.format(
+                timezone.now().strftime('%Y-%m-%d %H:%M:%S'), ae.email,
+                ae.user.email))
             subject = "PhysioNet Unverified Email Removal"
-            context = {'name':person.user.get_full_name(), 'email': person.email}
-            body = loader.render_to_string('user/email/unverified_email_removal.html', context)
+            context = {'name':ae.user.get_full_name(), 'email': ae.email}
+            body = loader.render_to_string(
+                'user/email/unverified_email_removal.html', context)
             send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
-                [person.user.email], fail_silently=False)
+                [ae.user.email], fail_silently=False)
+
 
 class RemoveOutstandingInvites(CronJobBase):
     RUN_EVERY_MINS = 1
     RETRY_AFTER_FAILURE_MINS = 5
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS, 
+    schedule = Schedule(run_every_mins=RUN_EVERY_MINS,
         retry_after_failure_mins=RETRY_AFTER_FAILURE_MINS)
 
     code = 'physionet.RemoveOutstandingInvites'
 
     def do(self):
-        invitations = Invitation.objects.filter(invitation_type='author', is_active=True, 
-            request_datetime__lt=timezone.now() - timezone.timedelta(days=TIME_LIMIT))
-        for invite in invitations:
-            invite.response_message = "Time limit for outstanding invitation passed."
-            invite.response = False
-            invite.response_datetime = timezone.now()
-            invite.is_active = False
-            invite.save()
-            subject = "PhysioNet Unconfirmed Author Removal"
-            context = {'name':invite.inviter.get_full_name(), 'email':invite.email,
-                'title':invite.project.title}
-            body = loader.render_to_string('project/email/unconfirmed_author_removal.html', context)
+        invitations = AuthorInvitation.objects.filter(is_active=True,
+            request_datetime__lt=timezone.now() - timezone.timedelta(
+                days=UNVERIFIED_DAY_LIMIT))
+
+        for invitation in invitations:
+            invitation.response_datetime = timezone.now()
+            invitation.is_active = False
+            invitation.save()
+            subject = 'PhysioNet Expired Author Invitation'
+            context = {'name':invitation.inviter.get_full_name(),
+                'email':invitation.email, 'title':invitation.project.title}
+            body = loader.render_to_string(
+                'project/email/outstanding_invitation_removal.html', context)
             send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
-                [invite.inviter.email], fail_silently=False)
-            print('{0}: Removed author invitation for project {1} from {2} to {3}'.format(
-                timezone.now().strftime('%Y-%m-%d %H:%M:%S'), invite.project.title, 
-                invite.inviter.email, invite.email))
+                [invitation.inviter.email], fail_silently=False)
+            print('{}: Removed author invitation for project {} from {} to {}'.format(
+                timezone.now().strftime('%Y-%m-%d %H:%M:%S'),
+                invitation.project.title, invitation.inviter.email,
+                invitation.email))
