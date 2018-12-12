fde93cad08be0863cbd97576fde4a1dc3755e43d
Chen Xie <is.chenx@gmail.com>
2018-12-12 12:45:44 -0500

    update to first names instead of middle

--- physionet-django/static/zxcvbn/zxcvbn_ProgressBar_Register.js
+++ physionet-django/static/zxcvbn/zxcvbn_ProgressBar_Register.js
@@ -1,8 +1,8 @@
 $("input").change(function()
 {
 	$("#StrengthProgressBar").zxcvbnProgressBar({
-		passwordInput: "#id_password1", 
-		userInputs: [$("#id_email").val(), $("#id_first_name").val(), 
-			$("#id_middle_names").val(), $("#id_last_name").val()]
+		passwordInput: "#id_password1",
+		userInputs: [$("#id_email").val(), $("#id_first_names").val(),
+			$("#id_last_name").val()]
 	});
 });
