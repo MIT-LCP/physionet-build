8bb35d50889f1f5315d2d3630584798f44117aa2
Chen Xie <is.chenx@gmail.com>
2019-02-20 15:30:20 -0500

    add link to available licenses in access form

--- physionet-django/project/forms.py
+++ physionet-django/project/forms.py
@@ -571,7 +571,7 @@ class AccessMetadataForm(forms.ModelForm):
         model = ActiveProject
         fields = ('access_policy', 'license')
         help_texts = {'access_policy': '* Access policy for files.',
-                      'license': '* License for usage'}
+                      'license': "* License for usage. <a href='/about/licenses/' target='_blank'>View available.</a>"}
 
     def __init__(self, include_credentialed, *args, **kwargs):
         """
