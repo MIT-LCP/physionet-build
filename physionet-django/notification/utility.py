9f9615974602d20b769b5d93cc9773a7b6ca3536
Tom Pollard <tpollard@mit.edu>
2019-02-27 14:13:45 -0500

    clean email text

--- physionet-django/notification/utility.py
+++ physionet-django/notification/utility.py
@@ -24,6 +24,38 @@ def send_contact_message(contact_form):
 
 # ---------- Project App ---------- #
 
+def email_signature():
+    """
+    Gets the signature for the emails
+    """
+    signature = ("Regards,\n\n"
+        "The PhysioNet Team,\n"
+        "MIT Laboratory for Computational Physiology,\n"
+        "Institute for Medical Engineering and Science,\n"
+        "45 Carleton St, Cambridge, MA 02142"
+        )
+
+    return signature
+
+def email_project_info(project):
+    """
+    Header for the email. e.g. Project ID, Project title, Submitting Author.
+    """
+    header = ("Project title: {}\n"
+        "Submission ID: {}\n"
+        "Submitting author: {}"
+        ).format(project.title, project.slug, project.submitting_author())
+
+    return header
+
+def email_footer():
+    """
+    Footer for the email. e.g. for privacy policy, link to update profile, etc.
+    """
+    footer = ""
+
+    return footer
+
 def invitation_notify(request, invite_author_form, target_email):
     """
     Notify someone when they are invited to author a project
@@ -34,12 +66,16 @@ def invitation_notify(request, invite_author_form, target_email):
     email_context = {'inviter_name':inviter.get_full_name(),
                      'inviter_email':inviter.email,
                      'project':project,
-                     'domain':get_current_site(request)}
+                     'domain':get_current_site(request),
+                     'signature':email_signature(),
+                     'project_info':email_project_info(project),
+                     'footer':email_footer()}
+
     body = loader.render_to_string('notification/email/invite_author.html',
                                     email_context)
+
     send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
                   [target_email], fail_silently=False)
-
 
 def invitation_response_notify(invitation, affected_emails):
     """
@@ -51,28 +87,33 @@ def invitation_response_notify(invitation, affected_emails):
 
     subject = 'Authorship invitation {} for project: {}'.format(response,
                                                                 project.title)
-    email, name = project.author_contact_info(only_submitting=True)
-    email_context = {'name':name, 'project':project,
-        'response':response}
+    email, firstname, fullname = project.author_contact_info(only_submitting=True)
+    email_context = {'name':firstname, 'project':project,
+        'response':response, 'signature':email_signature(),
+        'project_info':email_project_info(project),'footer':email_footer()}
+
     # Send an email for each email belonging to the accepting user
     for author_email in affected_emails:
         email_context['author_email'] = author_email
         body = loader.render_to_string(
             'notification/email/author_response.html', email_context)
+
         send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
                   [email], fail_silently=False)
-
 
 def submit_notify(project):
     """
     Notify authors when a project is submitted
     """
     subject = 'Submission of project: {}'.format(project.title)
-    email_context = {'project':project}
-    for email, name in project.author_contact_info():
-        email_context['name'] = name
+    email_context = {'project':project, 'signature':email_signature(),
+        'project_info':email_project_info(project),'footer':email_footer()}
+
+    for email, firstname, fullname in project.author_contact_info():
+        email_context['name'] = firstname
         body = loader.render_to_string(
             'notification/email/submit_notify.html', email_context)
+
         send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
                   [email], fail_silently=False)
 
@@ -82,17 +123,21 @@ def resubmit_notify(project):
     Notify authors and the editor when a project is resubmitted
     """
     subject = 'Resubmission of project: {}'.format(project.title)
-    email_context = {'project':project}
-    for email, name in project.author_contact_info():
-        email_context['name'] = name
+    email_context = {'project':project, 'signature':email_signature(),
+        'project_info':email_project_info(project),'footer':email_footer()}
+
+    for email, firstname, fullname in project.author_contact_info():
+        email_context['name'] = firstname
         body = loader.render_to_string(
             'notification/email/resubmit_notify.html', email_context)
+
         send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
                           [email], fail_silently=False)
 
     email_context['name'] = project.editor.get_full_name()
     body = loader.render_to_string(
         'notification/email/resubmit_notify_editor.html', email_context)
+
     send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
               [project.editor.email], fail_silently=False)
 
@@ -104,11 +149,16 @@ def assign_editor_notify(project):
     """
     subject = 'Editor assigned to submission of project: {0}'.format(
         project.title)
-    for email, name in project.author_contact_info():
+
+    for email, firstname, fullname in project.author_contact_info():
         body = loader.render_to_string(
             'notification/email/assign_editor_notify.html',
-            {'name':name, 'project':project,
-             'editor':project.editor})
+            {'name':firstname, 'project':project,
+             'editor':project.editor, 
+             'signature':email_signature(),
+             'project_info':email_project_info(project),
+             'footer':email_footer()})
+
         send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
                   [email], fail_silently=False)
 
@@ -129,10 +179,14 @@ def edit_decision_notify(request, project, edit_log):
         subject = 'Submission accepted for project: {}'.format(project.title)
         template = 'notification/email/accept_submission_notify.html'
 
-    for email, name in project.author_contact_info():
+    for email, firstname, fullname in project.author_contact_info():
         body = loader.render_to_string(template,
-            {'name':name, 'project':project, 'edit_log':edit_log,
-             'domain':get_current_site(request)})
+            {'name':firstname, 'project':project, 'edit_log':edit_log,
+             'domain':get_current_site(request),
+             'signature':email_signature(),
+             'project_info':email_project_info(project),
+             'footer':email_footer()})
+
         send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
                   [email], fail_silently=False)
 
@@ -142,11 +196,16 @@ def copyedit_complete_notify(request, project, copyedit_log):
     Notify authors when the editor has finished copyediting
     """
     subject = 'Copyedit complete for project: {0}'.format(project.title)
-    for email, name in project.author_contact_info():
+
+    for email, firstname, fullname in project.author_contact_info():
         body = loader.render_to_string(
             'notification/email/copyedit_complete_notify.html',
-            {'name':name, 'project':project, 'copyedit_log':copyedit_log,
-             'domain':get_current_site(request)})
+            {'name':firstname, 'project':project, 'copyedit_log':copyedit_log,
+             'domain':get_current_site(request),
+             'signature':email_signature(),
+             'project_info':email_project_info(project),
+             'footer':email_footer()})
+
         send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
                   [email], fail_silently=False)
 
@@ -156,25 +215,37 @@ def reopen_copyedit_notify(request, project):
     Notify authors when an editor reopens a project for copyediting
     """
     subject = 'Project reopened for copyediting: {0}'.format(project.title)
-    for email, name in project.author_contact_info():
+
+    for email, firstname, fullname in project.author_contact_info():
         body = loader.render_to_string(
             'notification/email/reopen_copyedit_notify.html',
-            {'name':name, 'project':project,
-             'domain':get_current_site(request)})
+            {'name':firstname, 'project':project,
+             'domain':get_current_site(request),
+             'signature':email_signature(),
+             'project_info':email_project_info(project),
+             'footer':email_footer()})
+
         send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
                   [email], fail_silently=False)
 
 def authors_approved_notify(request, project):
+    """
+    Notify ...
+    """
     subject = 'All authors approved publication of project: {0}'.format(
         project.title)
-    for email, name in project.author_contact_info():
+
+    for email, firstname, fullname in project.author_contact_info():
         body = loader.render_to_string(
             'notification/email/authors_approved_notify.html',
-            {'name':name, 'project':project,
-             'domain':get_current_site(request)})
+            {'name':firstname, 'project':project,
+             'domain':get_current_site(request),
+             'signature':email_signature(),
+             'project_info':email_project_info(project),
+             'footer':email_footer()})
+
         send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
                   [email], fail_silently=False)
-
 
 def publish_notify(request, published_project):
     """
@@ -182,14 +253,18 @@ def publish_notify(request, published_project):
     """
     subject = 'Your project has been published: {0}'.format(
         published_project.title)
-    for email, name in published_project.author_contact_info():
+
+    for email, firstname, fullname in published_project.author_contact_info():
         body = loader.render_to_string(
             'notification/email/publish_notify.html',
-            {'name':name, 'published_project':published_project,
-             'domain':get_current_site(request)})
+            {'name':firstname, 'published_project':published_project,
+             'domain':get_current_site(request),
+             'signature':email_signature(),
+             'project_info':email_project_info(published_project),
+             'footer':email_footer()})
+
         send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
                   [email], fail_silently=False)
-
 
 def storage_response_notify(storage_request):
     """
@@ -199,14 +274,17 @@ def storage_response_notify(storage_request):
     response = RESPONSE_ACTIONS[storage_request.response]
     subject = 'Storage request {0} for project: {1}'.format(response,
         project.title)
-    email, name = project.author_contact_info(only_submitting=True)
+    email, firstname, fullname = project.author_contact_info(only_submitting=True)
     body = loader.render_to_string('notification/email/storage_response_notify.html',
-        {'name':name, 'project':project, 'response':response,
+        {'name':firstname, 'project':project, 'response':response,
          'allowance':storage_request.request_allowance,
-         'response_message':storage_request.response_message})
+         'response_message':storage_request.response_message,
+         'signature':email_signature(),
+         'project_info':email_project_info(project),
+         'footer':email_footer()})
+
     send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
               [email], fail_silently=False)
-
 
 def contact_reference(request, application):
     """
@@ -217,17 +295,25 @@ def contact_reference(request, application):
         applicant_name)
     body = loader.render_to_string('notification/email/contact_reference.html',
         {'application':application, 'applicant_name':applicant_name,
-         'domain':get_current_site(request)})
+         'domain':get_current_site(request),
+         'signature':email_signature(),
+         'footer':email_footer()})
+
     send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
               [application.reference_email], fail_silently=False)
 
-
 def process_credential_complete(request, application):
+    """
+    Notify user of credentialing decision
+    """
     applicant_name = ' '.join([application.first_names, application.last_name])
     response = 'rejected' if application.status == 1 else 'accepted'
     subject = 'PhysioNet credentialling {}'.format(response)
     body = loader.render_to_string('notification/email/process_credential_complete.html',
         {'application':application, 'applicant_name':applicant_name,
-         'domain':get_current_site(request)})
+         'domain':get_current_site(request),
+         'signature':email_signature(),
+         'footer':email_footer()})
+
     send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
               [application.user.email], fail_silently=False)
