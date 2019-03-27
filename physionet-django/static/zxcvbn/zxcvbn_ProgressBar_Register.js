ae8daebf6cbb905d9b639b4f96182fb9a3623328
Benjamin Moody <benjaminmoody@gmail.com>
2019-03-27 14:40:45 -0400

    zxcvbn_ProgressBar_Register: devalue user/site-related terms.
    When a new user is registering, if the password includes elements of
    their username, their real name, or their email address, these
    elements should be treated as having low entropy (easier for an
    attacker to guess.)  Also, terms related to the site itself should
    also be treated as having low entropy.

--- physionet-django/static/zxcvbn/zxcvbn_ProgressBar_Register.js
+++ physionet-django/static/zxcvbn/zxcvbn_ProgressBar_Register.js
@@ -1,8 +1,16 @@
 $("input").change(function()
 {
+	// NOTE: Keep list of forbidden words in sync with
+	// zxcvbn_ProgressBar_Change.js and ComplexityValidator in
+	// validators.py
+
+	var t = ($("#id_email").val() + ' ' +
+	         $("#id_first_names").val() + ' ' +
+	         $("#id_last_name").val() + ' ' +
+	         $("#id_username").val() + ' physio physionet');
+
 	$("#StrengthProgressBar").zxcvbnProgressBar({
 		passwordInput: "#id_password1",
-		userInputs: [$("#id_email").val(), $("#id_first_names").val(),
-			$("#id_last_name").val()]
+		userInputs: t.match(/\d+|[^\W\d_]+/g)
 	});
 });
