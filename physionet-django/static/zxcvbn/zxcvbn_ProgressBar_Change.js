00d10aa0fe4aa9a66d59902539ba62626b098ae0
Benjamin Moody <benjaminmoody@gmail.com>
2019-03-27 14:45:07 -0400

    zxcvbn_ProgressBar_Change: devalue user/site-related terms.
    When a user is changing their password, if the new password includes
    elements of their username, their real name, or their (current
    primary) email address, these elements should be treated as having low
    entropy (easier for an attacker to guess.)  Also, terms related to the
    site itself should also be treated as having low entropy.

--- physionet-django/static/zxcvbn/zxcvbn_ProgressBar_Change.js
+++ physionet-django/static/zxcvbn/zxcvbn_ProgressBar_Change.js
@@ -1,11 +1,18 @@
 $(document).ready(function()
 {
+	// NOTE: Keep list of forbidden words in sync with
+	// zxcvbn_ProgressBar_Register.js and ComplexityValidator in
+	// validators.py
+
 	var name = document.getElementById("helper").getAttribute("data-name");
 	var last = document.getElementById("helper").getAttribute("data-last");
-	var url = document.getElementById("helper").getAttribute("data-url");
 	var email = document.getElementById("helper").getAttribute("data-email");
+	var user = document.getElementById("helper").getAttribute("data-username");
+	var t = (email + ' ' + name + ' ' + last + ' ' + user +
+	         ' physio physionet');
+
 	$("#StrengthProgressBar").zxcvbnProgressBar({
 		passwordInput: "#id_new_password1",
-		userInputs: [name, last, url, email]
+		userInputs: t.match(/\d+|[^\W\d_]+/g)
 	});
 });
