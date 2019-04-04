8e3c3851b81b315e0456ba2bcfda61b20feffa00
Tom Pollard <tpollard@mit.edu>
2019-04-04 14:12:12 -0400

    Update views.py

--- physionet-django/notification/views.py
+++ physionet-django/notification/views.py
@@ -15,7 +15,7 @@ def news(request):
     minmax = News.objects.all().aggregate(min=Min('publish_datetime'), max=Max('publish_datetime'))
     news_years = list(range(minmax['min'].year, minmax['max'].year+1))
     return render(request, 'notification/news.html',
-        {'year':'Latest ', 'news_pieces':news_pieces, 'news_years':news_years})
+        {'year':'Latest', 'news_pieces':news_pieces, 'news_years':news_years})
 
 def news_year(request, year):
     news_pieces = News.objects.filter(publish_datetime__year=int(year)).order_by('-publish_datetime')
