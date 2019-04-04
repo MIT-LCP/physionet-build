0224e4cb85c7f4a1389eedb7ceec6179be79f2b9
Benjamin Moody <benjaminmoody@gmail.com>
2019-04-04 14:05:34 -0400

    SafeHTMLField: new class.
    Having defined a whitelist of permitted HTML tags and attributes, we
    need to enforce these restrictions on the server side as well as on
    the client side.
    
    This is implemented by the SafeHTMLField class, which is mostly a
    drop-in replacement for ckeditor.fields.RichTextField.  When text is
    submitted, it is parsed and cleaned using Bleach.  The list of
    permitted tags, attributes, and style properties is derived
    automatically from the 'allowedContent' option, which must be defined
    in the django-ckeditor configuration (settings.CKEDITOR_CONFIGS).

--- physionet-django/project/models.py
+++ physionet-django/project/models.py
@@ -6,6 +6,7 @@ import uuid
 import pdb
 import pytz
 
+import bleach
 from ckeditor.fields import RichTextField
 from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
@@ -22,6 +23,81 @@ from .utility import (get_tree_size, get_file_info, get_directory_info,
 from .validators import validate_doi, validate_subdir, validate_version
 from user.validators import validate_alphaplus, validate_alphaplusplus
 from physionet.utility import zip_dir
+
+
+class SafeHTMLField(RichTextField):
+    """
+    An HTML text field that permits only "safe" content.
+
+    On the client side, this field is displayed as an interactive
+    WYSIWYG editor (see ckeditor.fields.RichTextField.)
+
+    On the server side, the HTML text is "cleaned" using the bleach
+    library to ensure that all tags are properly closed, entities are
+    well-formed, etc., and to remove or escape any unsafe tags or
+    attributes.
+
+    The permitted set of tags and attributes is generated from the
+    corresponding 'allowedContent' rules in settings.CKEDITOR_CONFIGS
+    (which also defines the client-side whitelisting rules and the set
+    of options that are visible to the user.)  For example:
+
+        'allowedContent': {
+            'a': {'attributes': ['href']},
+            'em': True,
+            '*': {'attributes': ['title']},
+        }
+
+    This would permit the use of 'a' and 'em' tags (all other tags are
+    forbidden.)  'a' tags are permitted to have an 'href' attribute,
+    and any tag is permitted to have a 'title' attribute.
+
+    NOTE: This class does not use ckeditor's 'disallowedContent'
+    rules.  Those rules can be used to perform tag/attribute
+    blacklisting on the client side, but will not be enforced on the
+    server side.
+    """
+
+    # The following protocols may be used in 'href', 'src', and
+    # similar attributes.
+    _protocols = ['http', 'https', 'ftp', 'mailto']
+
+    # The following attributes are forbidden on the server side even
+    # if permitted on client side.  (This is a kludge; permitting
+    # 'width' to be set on the client side makes editing tables
+    # easier.)
+    _attribute_blacklist = {('table', 'width')}
+
+    # The following CSS properties may be set via inline styles (but
+    # only on elements for which the 'style' attribute itself is
+    # permitted.)
+    _styles = ['text-align']
+
+    def __init__(self, config_name='default', strip=False,
+                 strip_comments=True, **kwargs):
+        super().__init__(config_name=config_name, **kwargs)
+
+        conf = settings.CKEDITOR_CONFIGS[config_name]
+        tags = []
+        attrs = {}
+        for (tag, props) in conf['allowedContent'].items():
+            if tag != '*':
+                tags.append(tag)
+            if isinstance(props, dict) and 'attributes' in props:
+                attrs[tag] = []
+                for attr in props['attributes']:
+                    if (tag, attr) not in self._attribute_blacklist:
+                        attrs[tag].append(attr)
+
+        self._cleaner = bleach.Cleaner(tags=tags, attributes=attrs,
+                                       styles=self._styles,
+                                       protocols=self._protocols,
+                                       strip=strip,
+                                       strip_comments=strip_comments)
+
+    def clean(self, value, model_instance):
+        value = self._cleaner.clean(value)
+        return super().clean(value, model_instance)
 
 
 class Affiliation(models.Model):
