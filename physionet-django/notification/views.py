72d09255c7556bb0532cd8e9e4ee9dd2cef0b8ac
Lucas Bulgarelli <lucas.bulgarelli@hotmail.com>
2019-04-04 13:18:07 -0400

    all news

--- physionet-django/notification/views.py
+++ physionet-django/notification/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.utils import timezone
+from django.db.models import Min, Max
 
 from .models import News
 
@@ -8,12 +9,19 @@ def news(request):
     """
     Redirect to news for current year
     """
-    return redirect('news_year', year=timezone.now().year)
+    news_pieces = News.objects.order_by('-publish_datetime')
+
+    # The year range of all the PN news in existence.
+    minmax = News.objects.all().aggregate(min=Min('publish_datetime'), max=Max('publish_datetime'))
+    news_years = list(range(minmax['min'].year, minmax['max'].year+1))
+    return render(request, 'notification/news.html',
+        {'year':'All', 'news_pieces':news_pieces, 'news_years':news_years})
 
 def news_year(request, year):
     news_pieces = News.objects.filter(publish_datetime__year=int(year)).order_by('-publish_datetime')
     # The year range of all the PN news in existence.
     # Yes, the start is hardcoded.
-    news_years = list(range(2019, timezone.now().year + 1))
+    minmax = News.objects.all().aggregate(min=Min('publish_datetime'), max=Max('publish_datetime'))
+    news_years = list(range(minmax['min'].year, minmax['max'].year+1))
     return render(request, 'notification/news.html',
         {'year':year, 'news_pieces':news_pieces, 'news_years':news_years})
