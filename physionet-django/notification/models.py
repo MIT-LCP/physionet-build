da6d7efb0b1e3bf8120736e87b5e94133752ce34
Tom Pollard <tpollard@mit.edu>
2019-04-17 02:15:07 -0400

    link news to project. ref #368

--- physionet-django/notification/models.py
+++ physionet-django/notification/models.py
@@ -10,6 +10,8 @@ class News(models.Model):
     content = SafeHTMLField()
     publish_datetime = models.DateTimeField(auto_now_add=True)
     url = models.URLField(default='', blank=True)
+    project = models.ForeignKey('project.PublishedProject', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='news')
 
     def __str__(self):
         return '{} - {}'.format(self.title, self.publish_datetime.date())
