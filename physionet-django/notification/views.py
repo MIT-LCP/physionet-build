d51b10a355af88affd1559fe1ca650ccd9f5d3f8
Benjamin Moody <benjaminmoody@gmail.com>
2019-05-01 20:24:03 -0400

    notification/views.py: PEP8 style cleanup.

--- physionet-django/notification/views.py
+++ physionet-django/notification/views.py
@@ -12,16 +12,22 @@ def news(request):
     news_pieces = News.objects.order_by('-publish_datetime')
 
     # The year range of all the PN news in existence.
-    minmax = News.objects.all().aggregate(min=Min('publish_datetime'), max=Max('publish_datetime'))
+    minmax = News.objects.all().aggregate(min=Min('publish_datetime'),
+                                          max=Max('publish_datetime'))
     news_years = list(range(minmax['min'].year, minmax['max'].year+1))
     return render(request, 'notification/news.html',
-        {'year':'Latest', 'news_pieces':news_pieces, 'news_years':news_years})
+                  {'year': 'Latest', 'news_pieces': news_pieces,
+                   'news_years': news_years})
+
 
 def news_year(request, year):
-    news_pieces = News.objects.filter(publish_datetime__year=int(year)).order_by('-publish_datetime')
+    news_pieces = News.objects.filter(publish_datetime__year=int(year)) \
+                              .order_by('-publish_datetime')
     # The year range of all the PN news in existence.
     # Yes, the start is hardcoded.
-    minmax = News.objects.all().aggregate(min=Min('publish_datetime'), max=Max('publish_datetime'))
+    minmax = News.objects.all().aggregate(min=Min('publish_datetime'),
+                                          max=Max('publish_datetime'))
     news_years = list(range(minmax['min'].year, minmax['max'].year+1))
     return render(request, 'notification/news.html',
-        {'year':year, 'news_pieces':news_pieces, 'news_years':news_years})
+                  {'year': year, 'news_pieces': news_pieces,
+                   'news_years': news_years})
