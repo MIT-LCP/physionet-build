fc2b7f865e3a3d867d71b8323cd700e61291ffc1
Lucas Bulgarelli <lucas.bulgarelli@hotmail.com>
2019-05-11 02:39:32 -0400

    add dependencies to settings

--- physionet-django/physionet/settings/base.py
+++ physionet-django/physionet/settings/base.py
@@ -29,6 +29,8 @@ SECRET_KEY = config('SECRET_KEY')
 # Application definition
 
 INSTALLED_APPS = [
+    'dal',
+    'dal_select2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
