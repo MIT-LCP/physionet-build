bc716dde7dedaa2bfc42c69a2fdd91531f34063c
Lucas Bulgarelli <lucas.bulgarelli@hotmail.com>
2019-05-11 04:56:25 -0400

    scrolling and footer

--- physionet-django/console/static/console/js/console.js
+++ physionet-django/console/static/console/js/console.js
@@ -16,13 +16,6 @@
     e.preventDefault();
     $("body").removeClass("sidenav-toggled");
   });
-  // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
-  $('body.fixed-nav .navbar-sidenav, body.fixed-nav .sidenav-toggler, body.fixed-nav .navbar-collapse').on('mousewheel DOMMouseScroll', function(e) {
-    var e0 = e.originalEvent,
-      delta = e0.wheelDelta || -e0.detail;
-    this.scrollTop += (delta < 0 ? 1 : -1) * 30;
-    e.preventDefault();
-  });
   // Scroll to top button appear
   $(document).scroll(function() {
     var scrollDistance = $(this).scrollTop();
