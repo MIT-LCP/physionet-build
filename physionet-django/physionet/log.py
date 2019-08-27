e6733a7f16babc5b8503fb5c9f2c6e273ece99fb
Benjamin Moody <benjaminmoody@gmail.com>
2019-08-26 20:55:01 -0400

    VerboseStreamHandler: new class.
    This generates the message that Django's AdminEmailHandler would have
    sent by email, and sends it to the configured output stream
    (e.g. terminal or log file.)

--- physionet-django/physionet/log.py
+++ physionet-django/physionet/log.py
@@ -1,4 +1,5 @@
 from copy import copy
+import logging
 
 from django.views import debug
 from django.utils import log
@@ -149,3 +150,26 @@ class SaferAdminEmailHandler(log.AdminEmailHandler):
         message = "%s\n\n%s" % (self.format(no_exc_record), reporter.get_traceback_text())
         html_message = reporter.get_traceback_html() if self.include_html else None
         self.send_mail(subject, message, fail_silently=True, html_message=html_message)
+
+
+class VerboseStreamHandler(logging.StreamHandler):
+    def emit(self, record):
+        try:
+            request = record.request
+        except Exception:
+            request = None
+
+        # Since we add a nicely formatted traceback on our own, create a copy
+        # of the log record without the exception data.
+        no_exc_record = copy(record)
+        no_exc_record.exc_info = None
+        no_exc_record.exc_text = None
+
+        if record.exc_info:
+            exc_info = record.exc_info
+        else:
+            exc_info = (None, record.getMessage(), None)
+
+        reporter = debug.ExceptionReporter(request, is_email=True, *exc_info)
+        message = "%s\n\n%s" % (self.format(no_exc_record), reporter.get_traceback_text())
+        self.stream.write(message)
