bf043e7aae6717ce6d029b2a06df7d13f58bd984
Benjamin Moody <benjaminmoody@gmail.com>
2019-09-05 19:41:16 -0400

    serve_published_project_file: permit HTTP authentication.
    This permits command-line tools to read data files from a
    restricted-access database, if the user has been granted access.  For
    example, assuming 'rgmark' has signed the DUA for the demoeicu
    project:
    
    wget --user rgmark --ask-password \
         http://localhost:8000/files/demoeicu/2.0.0/SHA256SUMS.txt

--- physionet-django/project/views.py
+++ physionet-django/project/views.py
@@ -1292,6 +1292,7 @@ def serve_published_project_file(request, project_slug, version,
     Works for open and protected. Not needed for open.
 
     """
+    utility.check_http_auth(request)
     try:
         project = PublishedProject.objects.get(slug=project_slug,
             version=version)
@@ -1313,7 +1314,8 @@ def serve_published_project_file(request, project_slug, version,
             return redirect(request.path + '/')
         except (NotADirectoryError, FileNotFoundError):
             raise Http404()
-    raise PermissionDenied()
+
+    return utility.require_http_auth(request)
 
 
 def display_published_project_file(request, project_slug, version,
