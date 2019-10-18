a3f11c59d8f6e0bc1d0f22d6f57b71a6db467261
Benjamin Moody <benjaminmoody@gmail.com>
2019-10-18 18:27:24 -0400

    Mark the session ID cookie as Secure.
    This means that the cookie must not be transmitted over unencrypted
    channels.
    
    (This is unnecessary if Strict-Transport-Security is used and if the
    cookie is only set for secure requests; but the "Secure" cookie
    attribute is much older and more widely supported than
    Strict-Transport-Security.)

--- physionet-django/physionet/settings/base.py
+++ physionet-django/physionet/settings/base.py
@@ -86,6 +86,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'physionet.wsgi.application'
 
+# Session management
+
+SESSION_COOKIE_SECURE = True
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
