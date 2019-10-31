feb610040a6faa3bedec5b2c05ccb78d911b4be2
Tom Pollard <tpollard@mit.edu>
2019-10-31 16:56:25 -0400

    simplify search

--- physionet-django/console/static/console/js/search-console.js
+++ physionet-django/console/static/console/js/search-console.js
@@ -2,15 +2,7 @@
 // e.g. search users, news items, etc.
 
 function search(url, value){
- var csrftoken = getCookie('csrftoken');
- $.ajax({
-         type: "POST",
-         url: url,
-         data: {'csrfmiddlewaretoken':csrftoken,
-                'search': value
-         },
-         success: function reloadSection(result){
-             $("#searchitems").html(result);
-         },
- });
+  var csrftoken = getCookie('csrftoken');
+  var data = {'csrfmiddlewaretoken':csrftoken, 'search': value};
+  $('#searchitems').load(url, data);
 }
