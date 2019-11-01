76698808004f7a79d304799152d1a16ac1499bfd
Tom Pollard <tpollard@mit.edu>
2019-11-01 18:08:20 -0400

    add timeout to search

--- physionet-django/console/static/console/js/search-console.js
+++ physionet-django/console/static/console/js/search-console.js
@@ -1,8 +1,20 @@
 // Script to search lists in the console
 // e.g. search users, news items, etc.
 
+// Init a timeout variable to be used below
+var loadresults = null;
+
 function search(url, value){
-  var csrftoken = getCookie('csrftoken');
-  var data = {'csrfmiddlewaretoken':csrftoken, 'search': value};
-  $('#searchitems').load(url, data);
+    var csrftoken = getCookie('csrftoken');
+
+    // Clear the timeout if it has already been set.
+    // This will prevent the previous task from executing
+    // if it has been less than <MILLISECONDS>
+    clearTimeout(loadresults);
+
+    // Make a new timeout set to go off in <N>ms
+    loadresults = setTimeout(function(){
+        var data = {'csrfmiddlewaretoken':csrftoken, 'search': value};
+        $('#searchitems').load(url, data);
+    }, 500);
 }
