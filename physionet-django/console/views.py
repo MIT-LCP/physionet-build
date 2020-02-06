5e83966d811e180bd870ed3bac15866de769bf06
Felipe Torres <felipe.torres.cs@gmail.com>
2020-02-06 11:07:18 -0600

    Add message if DataCite is not configured
    A simple message above the DOI read-only box will be displayed
    if the DataCite prefix is not in the .env file.
    
    This is useful for people reusing the platform, and for us to see
    why there is NO DOI.
    
    I am assuming that if you took the time to set the prefix, you
    also took the time to set the user/password.

--- physionet-django/console/views.py
+++ physionet-django/console/views.py
@@ -494,7 +494,8 @@ def publish_submission(request, project_slug, *args, **kwargs):
 
             current_site = Site.objects.get_current()
             url = 'https://{0}/content/{1}/{2}'.format(current_site, slug, project.version)
-            if utility.publish_doi_draft(url, project.doi):
+            if datacite_is_enabled:
+                utility.publish_doi_draft(url, project.doi)
                 messages.success(request, 'Successfully created DOI.')
             notification.publish_notify(request, published_project)
             return render(request, 'console/publish_complete.html',
@@ -502,9 +503,10 @@ def publish_submission(request, project_slug, *args, **kwargs):
 
     publishable = project.is_publishable()
     url_prefix = notification.get_url_prefix(request)
-    if project.doi == '':
+    if datacite_is_enabled and project.doi == '':
         project.doi = utility.create_doi_draft(project)
         project.save()
+
     publish_form = forms.PublishForm(project=project)
 
     return render(request, 'console/publish_submission.html',
