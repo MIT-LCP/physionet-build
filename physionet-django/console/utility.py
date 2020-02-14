a21f804ba21f9b22ffd3eb1eb401461ed083e419
Felipe Torres <felipe.torres.cs@gmail.com>
2020-02-14 07:29:29 -0600

    Treat the return get_doi_status as a string
    Do a proper check if the status of a DOI is a draft.
    There are 2 options:
    
    1. If the status is draft, then we can publish.
    2. If the status is publish, then there is no event to trigger
    and we just update.

--- physionet-django/console/utility.py
+++ physionet-django/console/utility.py
@@ -352,13 +352,17 @@ def publish_doi(project):
 
     The URL is made using the prefix and doi assigned to the project.
     """
-    if project.doi and get_doi_status(project.doi):
-        payload, url = generate_doi_info(project, publish=True)
+    publish = False
+    if project.doi:
+        if get_doi_status(project.doi) == "draft":
+            publish = True
+        payload, url = generate_doi_info(project, publish=publish)
         send_doi_update(url, payload)
-    if (project.core_project.doi and project.is_latest_version and
-            get_doi_status(project.core_project.doi)):
+    if project.core_project.doi and project.is_latest_version:
+        if get_doi_status(project.core_project.doi) == "draft":
+            publish = True
         payload, url = generate_doi_info(project, core_project=True,
-            publish=True)
+            publish=publish)
         send_doi_update(url, payload)
 
 
