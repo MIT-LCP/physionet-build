f28511a0c729a9f4605fc821e8005d6e379cef49
Felipe Torres <felipe.torres.cs@gmail.com>
2020-03-12 11:04:13 -0500

    Improve readability

--- physionet-django/project/views.py
+++ physionet-django/project/views.py
@@ -1600,12 +1600,14 @@ def project_request_access(request, project_slug, version, access_type):
     """
     user = request.user
     project = PublishedProject.objects.get(slug=project_slug, version=version)
+    data_access = DataAccess.objects.filter(project=project,
+                                            platform=access_type)
+
     # Check if the person has access to the project.
     if not project.has_access(request.user):
         return redirect('published_project', project_slug=project_slug,
             version=version)
-    data_access = DataAccess.objects.filter(project=project,
-        platform=access_type)
+
     try:
         # check user if user has GCP or AWS info in profile
         if (user.cloud_information.gcp_email is None and access_type in [3, 4]) or (
@@ -1615,6 +1617,7 @@ def project_request_access(request, project_slug, version, access_type):
     except CloudInformation.DoesNotExist:
         messages.error(request, 'Please set the user cloud information in your settings')
         return redirect('edit_cloud')
+
     for access in data_access:
         if access_type == 2 and user:
             message = utility.grant_aws_open_data_access(user, project)
