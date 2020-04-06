e31badd26d4945af1d8ff01a7091a2a825657115
Shulammite Lim <shulammitelim@gmail.com>
2020-04-06 16:00:10 -0500

    Add second line before next function

--- physionet-django/notification/utility.py
+++ physionet-django/notification/utility.py
@@ -387,6 +387,7 @@ def publish_notify(request, published_project):
         
     send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
           [settings.CONTACT_EMAIL], fail_silently=False)
+          
 
 def storage_response_notify(storage_request):
     """
