1c5fa0b90ae811421511569adbb3e74c0852aee0
Lucas McCullum <lumccul3@gmail.com>
2020-06-04 10:46:13 -0400

    Adds PublishedReference class

--- physionet-django/export/schema.py
+++ physionet-django/export/schema.py
@@ -1,7 +1,8 @@
 import graphene
+import pdb
 from graphene_django.types import DjangoObjectType
 
-from project.models import PublishedProject, PublishedAuthor
+from project.models import PublishedProject, PublishedAuthor, PublishedReference
 
 
 class PublishedProjectType(DjangoObjectType):
@@ -17,12 +18,21 @@ class PublishedAuthorType(DjangoObjectType):
         fields = ('first_names', 'last_name', 'corresponding_email', 'project')
 
 
+class PublishedReferenceType(DjangoObjectType):
+    class Meta:
+        model = PublishedReference
+
+
 class Query(object):
     all_published_projects = graphene.List(PublishedProjectType)
     all_published_authors = graphene.List(PublishedAuthorType)
+    all_published_references = graphene.List(PublishedReferenceType)
 
     def resolve_all_published_projects(self, info, **kwargs):
         return PublishedProject.objects.all()
 
     def resolve_all_published_authors(self, info, **kwargs):
         return PublishedAuthor.objects.all()
+
+    def resolve_all_published_references(self, info, **kwargs):
+        return PublishedReference.objects.all()
