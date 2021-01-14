ad1a838d666771adfa5b09f60d8c63dbd9a06dbd
Lucas McCullum <lumccul3@gmail.com>
2021-01-14 12:00:58 -0500

    Modifies credentialing application form #1182

--- physionet-django/user/forms.py
+++ physionet-django/user/forms.py
@@ -327,7 +327,7 @@ class PersonalCAF(forms.ModelForm):
             'organization_name', 'job_title', 'city', 'state_province',
             'zip_code', 'country', 'webpage')
         help_texts = {
-            'first_names': """You first name(s). This can be edited in your 
+            'first_names': """Your first name(s). This can be edited in your 
                 profile settings.""",
             'last_name': """Your last (family) name. This can be edited in 
                 your profile settings.""",
@@ -406,8 +406,11 @@ class TrainingCAF(forms.ModelForm):
         model = CredentialApplication
         fields = ('training_completion_report',)
         help_texts = {
-            'training_completion_report': """Upload the completion report from 
-                the CITI 'Data or Specimens Only Research' training program.""",
+            'training_completion_report': """Do not upload the completion 
+                certificate. Upload the completion report from the CITI 
+                'Data or Specimens Only Research' training program which 
+                lists all modules completed, with dates and scores. 
+                Expired reports will not be accepted.""",
         }
 
     def clean_training_completion_report(self):
@@ -429,7 +432,10 @@ class ReferenceCAF(forms.ModelForm):
             'reference_email', 'reference_title')
         help_texts = {
             'reference_category': """Your reference's relationship to you. If 
-                you are a student or postdoc, this must be your supervisor.""",
+                you are a student or postdoc, this must be your supervisor. 
+                Otherwise, you may list a colleague. Do not list yourself 
+                or another student as reference. Remind your reference to respond 
+                promptly as long response times will prevent application approval.""",
             'reference_name': 'The full name of your reference.',
             'reference_email': 'The email address of your reference.',
             'reference_title': "Your reference's professional title or position."
