3eaa7385a0f121c84e69946261bfc7264448d429
Lucas McCullum <lumccul3@gmail.com>
2021-02-04 12:34:08 -0500

    Adds indicator of known reference #1233

--- physionet-django/console/views.py
+++ physionet-django/console/views.py
@@ -8,6 +8,7 @@ from itertools import chain
 from statistics import median, StatisticsError
 from collections import OrderedDict
 
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.validators import validate_email
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test
@@ -1157,6 +1158,15 @@ def process_credential_application(request, application_slug):
                                                send=False,  wordwrap=False)
     contact_cred_ref_form = forms.ContactCredentialRefForm(initial=ref_email)
 
+    try:
+        ref = User.objects.get(associated_emails__email=application.reference_email.lower(),
+                               associated_emails__is_verified=True)
+        known_active = True
+        known_cred = ref.is_credentialed
+    except ObjectDoesNotExist:
+        known_active = False
+        known_cred = False
+
     page_title = None
     title_dict = {a: k for a, k in CredentialReview.REVIEW_STATUS_LABELS}
     page_title = title_dict[application.credential_review.status]
@@ -1363,7 +1373,8 @@ def process_credential_application(request, application_slug):
          'intermediate_credential_form': intermediate_credential_form,
          'process_credential_form': process_credential_form,
          'processing_credentials_nav': True, 'page_title': page_title,
-         'contact_cred_ref_form': contact_cred_ref_form})
+         'contact_cred_ref_form': contact_cred_ref_form,
+         'known_active': known_active, 'known_cred': known_cred})
 
 
 @login_required
