8ee942ea65a9818d32468bdc68bfee87279230d9
Chen Xie <is.chenx@gmail.com>
2021-07-02 23:28:50 -0700

    Use S3StaticStorage class

--- physionet-django/physionet/storage.py
+++ physionet-django/physionet/storage.py
@@ -1,11 +1,11 @@
 from django.conf import settings
-from storages.backends.s3boto3 import S3Boto3Storage
+from storages.backends.s3boto3 import S3Boto3Storage, S3StaticStorage
 
 
 class MediaStorage(S3Boto3Storage):
     bucket_name = settings.AWS_STORAGE_BUCKET_NAME
     location = ''
 
-class StaticStorage(S3Boto3Storage):
+class StaticStorage(S3StaticStorage):
     bucket_name = settings.AWS_STATIC_BUCKET_NAME
     location = ''
