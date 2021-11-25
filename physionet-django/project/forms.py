a9fcd9da37a83b03cfc0e39e154acba77c329785
Rafał Fiedosiuk <rafal.fiedosiuk@skygate.io>
2021-11-25 15:05:37 +0100

    Add missing quote.

--- physionet-django/project/forms.py
+++ physionet-django/project/forms.py
@@ -1079,7 +1079,7 @@ class EthicsForm(forms.ModelForm):
                 'and typically describes formal approvals acquired for '
                 'the creation of the resource (such as a review by an ethics board) '
                 'for users of the resource. If no concerns, please indicate '
-                'this “The authors declare no ethics concerns.'
+                'this “The authors declare no ethics concerns".'
             ),
         }
 
