ac164e9313d9cd7e13c435d54a324ed81be426bb
Lucas Bulgarelli <lucas.bulgarelli@hotmail.com>
2021-12-02 13:46:04 -0800

    update modified_datetime when editing section of unpublished project

--- physionet-django/project/modelcomponents/section.py
+++ physionet-django/project/modelcomponents/section.py
@@ -47,6 +47,58 @@ class SectionContent(models.Model):
         text = unescape(strip_tags(self.section_content))
         return text and not text.isspace()
 
+    @classmethod
+    def from_db(cls, *args, **kwargs):
+        """
+        Instantiate an object from the database.
+        """
+        instance = super(SectionContent, cls).from_db(*args, **kwargs)
+
+        # Save the original field values so that we can later check if
+        # they have been modified.  Note that by using __dict__, this
+        # will omit any deferred fields.
+        instance.orig_content = instance.section_content
+        return instance
+
+    def save(self, *, content_modified=None,
+             force_insert=False, update_fields=None, **kwargs):
+
+        # Use default behaviour if not dealing with UnpublishedProject
+        from project.models import UnpublishedProject
+        if not isinstance(self.project, UnpublishedProject):
+            return super().save(force_insert=force_insert,
+                                update_fields=update_fields,
+                                **kwargs)
+
+        # Note: UnpublishedProject.modified_datetime is an auto_now field,
+        # so it is automatically set to the current time (unless we exclude it
+        # using update_fields.)
+
+        if force_insert or update_fields:
+            # If force_insert is specified, then we want to insert a
+            # new object, which means setting the timestamp.  If
+            # update_fields is specified, then we want to update
+            # precisely those fields.  In either case, use the default
+            # save method.
+            content_modified = True
+
+        if hasattr(self, 'orig_content'):
+            # Check whether any of the Metadata fields have been
+            # modified since the object was loaded from the database.
+            if self.orig_content != self.section_content:
+                content_modified = True
+        else:
+            # If the object was not initially created by from_db,
+            # assume content has been modified.
+            content_modified = True
+
+        if content_modified:
+            # If content has been modified, then save normally.
+            self.project.save(update_fields=["modified_datetime"])
+        return super().save(force_insert=force_insert,
+                            update_fields=update_fields,
+                            **kwargs)
+
 
 class PublishedSectionContent(SectionContent):
     project = models.ForeignKey('project.PublishedProject',
