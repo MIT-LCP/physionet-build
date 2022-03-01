63ba3af81e794f66f40a0531e0769873222c39f7
Brian Gow <briangow@mit.edu>
2022-03-01 17:24:34 -0500

    Fixing the number of blank lines in the migration file continued

--- physionet-django/physionet/migrations/0003_auto_20220228_1456.py
+++ physionet-django/physionet/migrations/0003_auto_20220228_1456.py
@@ -3,8 +3,8 @@
 from django.db import migrations, models
 import django.db.models.deletion
 
-class Migration(migrations.Migration):
 
+class Migration(migrations.Migration):
 
     MIGRATE_AFTER_INSTALL = True
 
