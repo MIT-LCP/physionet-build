a51cf96319c2c858ccdcb0940f17b38c734f7871
Tom Pollard <tpollard@mit.edu>
2022-05-11 10:27:17 -0400

    rm unnecessary comment

--- physionet-django/user/views.py
+++ physionet-django/user/views.py
@@ -771,9 +771,6 @@ def credential_reference(request, application_slug):
     """
     Page for a reference to verify or reject a credential application
     """
-    # application = CredentialApplication.objects.filter(
-    #     slug=application_slug, reference_contact_datetime__isnull=False,
-    #     reference_response_datetime=None)
     application = CredentialApplication.objects.filter(
         slug=application_slug, reference_response_datetime=None)
 
