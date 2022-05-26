8d17a676cac18b0d3c743e26a1a9e6354f211c21
Brian Gow <briangow@mit.edu>
2022-05-26 16:48:52 -0400

    Migration for new datetime_added Orcid user model field

---
+++ physionet-django/user/migrations/0044_orcid_datetime_added.py
@@ -0,0 +1,31 @@
+from django.db import migrations, models
+import django.utils.timezone
+from datetime import datetime, timedelta
+
+
+def migrate_forward(apps, schema_editor):
+    orcid_model = apps.get_model('user', 'Orcid')
+    for row in orcid_model.objects.all():
+        row.datetime_added = datetime.fromtimestamp(row.token_expiration) - timedelta(days=20*365.2422)
+        row.save()
+
+
+def migrate_backward(apps, schema_editor):
+    pass
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('user', '0043_auto_20220406_1229'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='orcid',
+            name='datetime_added',
+            field=models.DateTimeField(auto_now_add=True, default=django.utils.timezone.now),
+            preserve_default=False,
+        ),
+        migrations.RunPython(migrate_forward, migrate_backward),
+    ]
