cfdc6443bd5294221aaa960bd84f553747de1fdb
Benjamin Moody <benjaminmoody@gmail.com>
2022-10-24 15:51:46 -0400

    notification.urls: enable automated URL testing.

--- physionet-django/notification/urls.py
+++ physionet-django/notification/urls.py
@@ -9,3 +9,9 @@ urlpatterns = [
     path('news/post/<int:news_id>', views.news_by_id, name='news_by_id'),
     path('feed.xml', views.news_rss, name='news_rss'),
 ]
+
+# Parameters for testing URLs (see physionet/test_urls.py)
+TEST_DEFAULTS = {
+    'year': '2018',
+    'news_id': '1',
+}
