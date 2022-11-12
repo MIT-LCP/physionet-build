7095693099e3a5df52dab6958d3b6b04153518a1
rutvikrj26 <rutvikrj26@gmail.com>
2022-11-12 16:24:25 -0500

    Adding license & DUA - still IntEnums(Further Work Required)

--- physionet-django/project/serializers.py
+++ physionet-django/project/serializers.py
@@ -1,16 +1,29 @@
 # from user.models import User
 from rest_framework import serializers
 
-from project.models import PublishedProject
+from project.models import PublishedProject, License, DUA
 
+#adding License & Dua for details
+class LicenseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = License
+        fields = ('name', 'access_policy', 'is_active')
+
+class DUASerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DUA
+        fields = ('name', 'access_policy', 'is_active')
 
 class PublishedProjectSerializer(serializers.ModelSerializer):
+    license = LicenseSerializer()
+    dua = DUASerializer()
     class Meta:
         model = PublishedProject
-        fields = ('id', 'title', 'abstract')
+        fields = ('id', 'title', 'abstract', 'license', 'dua')
 
 
 class PublishedProjectDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = PublishedProject
-        fields = ("title", "abstract", "background", "methods", "content_description", "usage_notes", "installation", "acknowledgements", "conflicts_of_interest", "release_notes", "ethics_statement", "version",  "short_description", "project_home_page", "publish_datetime", "doi", "slug")
+        fields = ("title", "abstract", "background", "methods", "content_description", "usage_notes", "installation", "acknowledgements", "conflicts_of_interest",
+         "release_notes", "ethics_statement", "version", "short_description", "project_home_page", "publish_datetime", "doi", "slug")
