2dd99585227d69e8515b0e7bf4452f6221d26249
rutvikrj26 <rutvikrj26@gmail.com>
2022-12-07 16:38:45 -0500

    Final License & dua, fixed styling issues

--- physionet-django/project/views.py
+++ physionet-django/project/views.py
@@ -2336,7 +2336,7 @@ class PublishedProjectList(mixins.ListModelMixin, generics.GenericAPIView):
     """
     queryset = PublishedProject.objects.all().order_by('id')
     authentication_classes = [SessionAuthentication, BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
     serializer_class = PublishedProjectSerializer
 
     def get(self, request, *args, **kwargs):
@@ -2349,7 +2349,7 @@ class PublishedProjectDetail(mixins.RetrieveModelMixin, generics.GenericAPIView)
     """
 
     authentication_classes = [SessionAuthentication, BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
 
     def get_object(self, slug, version):
         return get_object_or_404(PublishedProject, slug=slug, version=version)
