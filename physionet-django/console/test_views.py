cba93f9ab4d2ffaa42a5d4d0c4c4fb30f221fd50
Dana <dana@Danas-MacBook-Pro.local>
2022-12-14 13:59:16 -0500

    adjusts status code to redirect 302

--- physionet-django/console/test_views.py
+++ physionet-django/console/test_views.py
@@ -385,7 +385,7 @@ class TestState(TestMixin):
                               password=self.AUTHOR_PASSWORD)
             response = self.client.post(reverse('new_project_version', args=(self.PROJECT_SLUG,)),
                                         data={'version': version})
-            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.status_code, 302)
             project = ActiveProject.objects.get(title='MIT-BIH Arrhythmia Database')
             self.client.post(reverse('project_files', args=(project.slug,)), data={
                 'has_copy_right_permission': '0', 'has_human_subject_data': '0',
