82297370ce4b22200ea50468f215a1a888ddcaff
Dana Moukheiber <danamouk@mit.edu>
2022-12-14 19:56:58 -0500

    adds import

--- physionet-django/events/views.py
+++ physionet-django/events/views.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
+from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 
 import notification.utility as notification
