0d81dc81d078bba613f5ca1b698acf2db5226242
rutvikrj26 <rutvikrj26@gmail.com>
2023-01-08 12:51:41 -0500

    Moved the project serializers, views & urls to Export App, Removed the original views & serializers in Export

--- physionet-django/export/serializers.py
+++ physionet-django/export/serializers.py
@@ -1,10 +1,32 @@
+
 from rest_framework import serializers
 
-from project.models import PublishedProject
+from project.models import PublishedProject, License, DUA
+
+
+class LicenseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = License
+        fields = ('name',)
+
+
+class DUASerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DUA
+        fields = ('name',)
 
 
 class PublishedProjectSerializer(serializers.ModelSerializer):
+    license = LicenseSerializer()
+    dua = DUASerializer()
+
     class Meta:
         model = PublishedProject
-        fields = ('title', 'version', 'slug', 'abstract', 'main_storage_size',
-            'compressed_storage_size')
+        fields = ('id', 'title', 'abstract', 'license', 'dua')
+
+
+class PublishedProjectDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PublishedProject
+        fields = ("title", "abstract", "version", "short_description",
+                  "project_home_page", "publish_datetime", "doi", "slug")
--- physionet-django/export/urls.py
+++ physionet-django/export/urls.py
@@ -4,17 +4,10 @@ from export import views
 
 
 urlpatterns = [
-    path('rest/database-list/', views.database_list,
-        name='database_list'),
-    path('rest/software-list/', views.software_list,
-        name='software_list'),
-    path('rest/challenge-list/', views.challenge_list,
-        name='challenge'),
-    path('rest/model-list/', views.model_list,
-        name='model'),
-    path('rest/published-stats-list/', views.published_stats_list,
-        name='published_stats_list'),
+    # API V1 Routes
+    path('v1/published', views.PublishedProjectList.as_view(), name='Published_project_list'),
+    path('v1/published/<str:slug>/<str:version>', views.PublishedProjectDetail.as_view(),
+         name='Published_project_detail'),
 ]
-
 # Parameters for testing URLs (see physionet/test_urls.py)
 TEST_DEFAULTS = {}
