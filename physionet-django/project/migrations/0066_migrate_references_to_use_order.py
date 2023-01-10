59cf4924c9106f3ac7f59b6a870b978428882a53
Tom Pollard <tpollard@mit.edu>
2023-01-10 12:52:56 -0500

    migrate reference objects to use the new order field.

---
+++ physionet-django/project/migrations/0066_migrate_references_to_use_order.py
@@ -0,0 +1,63 @@
+from django.db import migrations, models
+from project.models import PublishedProject, ActiveProject, ArchivedProject, LegacyProject
+
+
+def migrate_forward(apps, schema_editor):
+
+    for project in PublishedProject.objects.all():
+        refs = project.references.all().order_by('id')
+        for n, ref in enumerate(refs):
+            ref.order = n + 1
+            ref.save()
+
+    for project in ActiveProject.objects.all():
+        refs = project.references.all().order_by('id')
+        for n, ref in enumerate(refs):
+            ref.order = n + 1
+            ref.save()
+
+    for project in ArchivedProject.objects.all():
+        refs = project.references.all().order_by('id')
+        for n, ref in enumerate(refs):
+            ref.order = n + 1
+            ref.save()
+
+    for project in LegacyProject.objects.all():
+        refs = project.references.all().order_by('id')
+        for n, ref in enumerate(refs):
+            ref.order = n + 1
+            ref.save()
+
+
+def migrate_backward(apps, schema_editor):
+    pass
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contenttypes', '0002_remove_content_type_name'),
+        ('project', '0065_editor_permissions'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='publishedreference',
+            name='order',
+            field=models.PositiveIntegerField(null=True),
+        ),
+        migrations.AddField(
+            model_name='reference',
+            name='order',
+            field=models.PositiveIntegerField(null=True),
+        ),
+        migrations.AlterUniqueTogether(
+            name='publishedreference',
+            unique_together={('description', 'project', 'order')},
+        ),
+        migrations.AlterUniqueTogether(
+            name='reference',
+            unique_together={('description', 'content_type', 'order')},
+        ),
+        migrations.RunPython(migrate_forward, migrate_backward),
+    ]
