8e046e6dbce86212b5f32d92babeb6d61b61a199
Dana Moukheiber <danamouk@mit.edu>
2023-01-11 16:35:45 -0500

    remove event models from user model

--- physionet-django/user/models.py
+++ physionet-django/user/models.py
@@ -21,8 +21,6 @@ from django.utils import timezone
 from django.utils.crypto import constant_time_compare
 from django.utils.translation import gettext as _
 
-from events.enums import EventCategory
-from events import validators
 from project.validators import validate_version
 from project.modelcomponents.access import AccessPolicy
 from project.modelcomponents.fields import SafeHTMLField
@@ -1210,52 +1208,3 @@ class CodeOfConductSignature(models.Model):
 
     class Meta:
         default_permissions = ()
-
-
-class Event(models.Model):
-    """
-    Captures information on events such as datathons, workshops and classes.
-    Used to allow event hosts to assist with credentialing.
-    """
-    title = models.CharField(max_length=64)
-    description = models.TextField(blank=True, null=True)
-    category = models.CharField(choices=EventCategory.choices, max_length=32)
-    host = models.ForeignKey(User, related_name='old_events', on_delete=models.CASCADE)
-    added_datetime = models.DateTimeField(auto_now_add=True)
-    start_date = models.DateField(default=timezone.now)
-    end_date = models.DateField(default=timezone.now)
-    slug = models.SlugField(unique=True, default=get_random_string)
-    allowed_domains = models.CharField(blank=True, null=True, validators=[
-                                       validators.validate_domain_list], max_length=100)
-
-    class Meta:
-        unique_together = ('title', 'host')
-        permissions = [('view_all_events', 'Can view all events in the console'),
-                       ('view_event_menu', 'Can view event menu in the navbar')]
-
-    def __str__(self):
-        return self.title
-
-    def enroll_user(self, user):
-        """
-        Adds a participant to an event.
-        """
-        with transaction.atomic():
-            EventParticipant.objects.create(user=user, event=self)
-            permission = Permission.objects.get(codename="view_event_menu")
-            user.user_permissions.add(permission)
-
-
-class EventParticipant(models.Model):
-    """
-    Captures information about participants in an event.
-    """
-    user = models.ForeignKey(User, related_name='old_event_participants', on_delete=models.CASCADE)
-    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='participants')
-    added_datetime = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        unique_together = ('user', 'event')
-
-    def __str__(self):
-        return self.user.get_full_name()
