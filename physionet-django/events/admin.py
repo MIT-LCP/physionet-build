f4410961fac6431f5614702b68f872d7d4a9eac0
Dana Moukheiber <danamouk@mit.edu>
2023-01-11 15:56:25 -0500

    reorganizes admin,validators,enums as part of events app

--- physionet-django/events/admin.py
+++ physionet-django/events/admin.py
@@ -1,3 +1,7 @@
+from events import models
 from django.contrib import admin
 
 # Register your models here.
+# Register the Event and Participants on the site
+admin.site.register(models.Event)
+admin.site.register(models.EventParticipant)
