8ababf09e2d8f3b1a985009217444dd3a1a815bd
lamam <lamawmouk@gmail.com>
2023-01-12 16:05:08 -0500

    fixes styling

--- physionet-django/user/validators.py
+++ physionet-django/user/validators.py
@@ -271,6 +271,6 @@ def validate_institutional_email(value):
     """
     Validate that the email address is from an institutional domain.
     """
-    domains= ["yahoo.com", "163.com", "126.com", "outlook.com", "gmail.com", "qq.com", "foxmail.com"]
+    domains = ["yahoo.com", "163.com", "126.com", "outlook.com", "gmail.com", "qq.com", "foxmail.com"]
     if value.split('@')[-1] in domains:
         raise ValidationError('Please provide an academic or institutional email address.')
