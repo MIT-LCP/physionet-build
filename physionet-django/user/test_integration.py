a8d5f46ab0d2f095ddd242084f9220d51326247d
lamam <lamawmouk@gmail.com>
2023-01-24 14:39:16 -0500

    Updates the test case for research summary for comaptibility with the form validation

--- physionet-django/user/test_integration.py
+++ physionet-django/user/test_integration.py
@@ -211,7 +211,7 @@ class TestCredentialing(TestMixin):
             'application-reference_email': 'root@example.com',
             'application-reference_organization': 'MIT',
             'application-reference_title': 'Administrator',
-            'application-research_summary': 'Effects of asdfghjk on zxcvbnm',
+            'application-research_summary': 'I am a researcher '*5,
         }
 
         self.client.post(reverse('credential_application'), data=data)
