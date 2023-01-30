de57bc86479d053a40b3ef3c18863ec30933c299
Amit Upreti <a.u.aua937@gmail.com>
2023-01-30 18:35:32 -0500

    Adding a test case to check management command
    I made a copy of `test_accept_pending_requests.py` and modified it to test the `reject_pending_credentialing_applications` management command.
    
    Quick note: The django recommendation to test the management command is to use `call_command` and not to test the `handle` method directly. https://docs.djangoproject.com/en/4.1/topics/testing/tools/#management-commands
    
    I think this is because the `handle` method is not intended to be called directly and since the  goal of the command should be to test the command itself, not the `handle` method.
    
    However, on this commit, i am testing the `handle` method directly, just in case there is some reason that we did it this way in the past.

---
+++ physionet-django/user/management/commands/test_reject_pending_credentialing_applications.py
@@ -0,0 +1,48 @@
+import logging
+
+from django.conf import settings
+from django.utils import timezone
+
+from user.management.commands import reject_pending_credentialing_applications
+from user.models import CredentialApplication
+from user.test_views import TestMixin
+
+LOGGER = logging.getLogger(__name__)
+
+
+def get_application_to_be_rejected():
+    """
+    Get all CredentialApplication that has been pending for more than
+    settings.MAX_REFERENCE_VERIFICATION_DAYS_BEFORE_AUTO_REJECTION days.
+    """
+    today = timezone.now()
+    limit = today - timezone.timedelta(days=settings.MAX_REFERENCE_VERIFICATION_DAYS_BEFORE_AUTO_REJECTION)
+
+    # get all applications that have decision pending
+    applications = CredentialApplication.objects.filter(status=0)
+    # get applications with Reference Response pending
+    response_applications = applications.filter(credential_review__status=30)
+    # finally get applications that have been pending for more than the limit
+    filtered_applications = response_applications.filter(reference_contact_datetime__lt=limit)
+    return filtered_applications
+
+
+class TestRejectPendingCredentialingApplications(TestMixin):
+
+    def test_rejection(self):
+
+        # load list of applications to be rejected
+        applications = get_application_to_be_rejected()
+        LOGGER.info(f'Found {len(applications)} applications to be rejected.')
+
+        # call the management command to auto reject applications
+        reject_pending_credentialing_applications.Command().handle()
+
+        # check if the applications are rejected
+        for application in applications:
+            application.refresh_from_db()
+            # rejected applications should have status 1
+            self.assertEqual(application.status, 1)
+
+        applications = get_application_to_be_rejected()
+        self.assertEqual(len(applications), 0)
