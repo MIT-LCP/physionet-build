c2f54954ae7e33c7ccebf2c7c9e519ff84a6835a
Benjamin Moody <benjaminmoody@gmail.com>
2023-01-31 16:09:21 -0500

    export.urls: URLs representing "directories" should end in a slash.
    A URL representing a directory - i.e., a resource that has, or might
    someday have, "child" resources - should, for consistency, end in a
    slash.

--- physionet-django/export/urls.py
+++ physionet-django/export/urls.py
@@ -5,10 +5,10 @@ from export import views
 
 urlpatterns = [
     # API V1 Routes
-    path('v1/project/published', views.PublishedProjectList.as_view(), name='Published_project_list'),
-    path('v1/project/published/<str:project_slug>', views.ProjectVersionList.as_view(),
+    path('v1/project/published/', views.PublishedProjectList.as_view(), name='Published_project_list'),
+    path('v1/project/published/<str:project_slug>/', views.ProjectVersionList.as_view(),
          name='Published_Project_versions'),
-    path('v1/project/published/<str:project_slug>/<str:version>', views.PublishedProjectDetail.as_view(),
+    path('v1/project/published/<str:project_slug>/<str:version>/', views.PublishedProjectDetail.as_view(),
          name='Published_project_detail'),
 ]
 
