5f3e9a4e3f2cdcc28f41f26ea4da201fd8ebb55a
Amit Upreti <a.u.aua937@gmail.com>
2023-03-13 15:07:01 -0400

    Implemented management command
    Added management command to reject pending registration
    applications for events that have ended.
    an email notification is sent to the applicant.

--- physionet-django/events/management/commands/reject_past_event_applications.py
+++ physionet-django/events/management/commands/reject_past_event_applications.py
@@ -6,15 +6,38 @@ from django.db import transaction
 from django.http import HttpRequest
 from django.utils import timezone
 
+from events.models import Event, EventApplication
 import notification.utility as notification
 
 LOGGER = logging.getLogger(__name__)
+
+AUTO_REJECTION_REASON = 'Event has ended.'
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         """
-        Auto reject pending registration applications for events that are older than
-        settings.MAX_EVENT_DAYS_BEFORE_AUTO_REJECTION
+        Auto reject pending registration applications for events that have ended
         """
-        pass
+        # creating an instance of HttpRequest to be used in the notification utility
+        request = HttpRequest()
+
+        past_events = Event.objects.filter(
+            end_date__lt=timezone.now(),
+            applications__status=EventApplication.EventApplicationStatus.WAITLISTED)
+
+        LOGGER.info(f'{past_events.count()} events selected for auto rejection of waitlisted applications.')
+
+        for event in past_events:
+            applications = event.applications.filter(status=EventApplication.EventApplicationStatus.WAITLISTED)
+            for application in applications:
+                with transaction.atomic():
+                    application.reject(comment_to_applicant=AUTO_REJECTION_REASON)
+                    notification.notify_participant_event_decision(
+                        request=request,
+                        user=application.user,
+                        event=application.event,
+                        decision=EventApplication.EventApplicationStatus.NOT_APPROVED.label,
+                        comment_to_applicant=AUTO_REJECTION_REASON
+                    )
+                    LOGGER.info(f'Application {application.id} for event {event.id} rejected.')
