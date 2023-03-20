218a955801560335c3d899f84dc4ad91039a1362
Amit Upreti <a.u.aua937@gmail.com>
2023-03-20 16:38:40 -0400

    implement trainings for user
    Allows users to take the test from settings.
    
    The quiz.html file loads all the content and questions based on the order. All the data is hidden from user and gradually displayed as users go through the training
    
    We have two level of verifications of answers. the first one is done on quiz.html where users go through the content and they will see the related questions and answer the questions. users can go to next section or skip the question only if they have answered the question correctly.
    We also have another verification done on view. here if the users had correctly answered the questions a json string is generated and sent via POST, we will then check the database to verify that the data in json string(question,answer) matched with the database. This should help us stop someone from just sending a post request to complete the training.
    
    Quick note:
    1. Users are not able to resume the test if they refresh the browser
    2. Users can take the same test multiple times
    3. Only multiple choice questions is supported yet.

---
+++ physionet-django/static/custom/js/quiz.js
@@ -0,0 +1,99 @@
+var slideIndex = 1;
+sessionStorage.setItem("slidePosition", 0);
+
+showSlide(slideIndex);
+
+function changeSlide(n) {
+    showSlide(slideIndex += n);
+}
+
+$('.next').on('click', function () {
+    // move to next slide but keep track of start of partaining training
+    let slidePosition = parseInt(sessionStorage.getItem("slidePosition"));
+    sessionStorage.setItem("slidePosition", slidePosition - 1)
+    changeSlide(1);
+})
+
+$('.previous').on('click', function () {
+    // move to next slide but keep track of start of partaining training
+    let slidePosition = parseInt(sessionStorage.getItem("slidePosition"));
+    sessionStorage.setItem("slidePosition", slidePosition - 2);
+    changeSlide(-1);
+})
+
+function showSlide(n) {
+
+    let i;
+    let slides = document.getElementsByClassName("eachQuiz");
+    if (n > slides.length) {
+        alertBox("<strong>Congratulations</strong> You have come to the end of the training.");
+        sessionStorage.clear();
+
+        let question_answers = {};
+        document.querySelectorAll('.question input[type="radio"]:checked').forEach(function (eachQuiz) {
+            let question_id = eachQuiz.getAttribute("name");
+            let value = eachQuiz.value;
+            question_answers[question_id] = parseInt(value);
+        });
+
+        $('[name="question_answers"]').val(JSON.stringify(question_answers));
+
+        $('form').submit();
+    }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+}
+//
+// $("input[type=radio]").on("click", function () {
+//     // check if answer is correct and proceed, else take back to training
+//     let id = $(this).attr("name")
+//     let value = $(this).val();
+//     var slidePosition = parseInt(sessionStorage.getItem("slidePosition"))
+//
+//     sessionStorage.setItem("slidePosition", 0)
+//
+//     if (sessionStorage.getItem(id) == value) {
+//       alertBox("<strong>Yay!</strong> You chose the correct answer.", "success")
+//       changeSlide(1)
+//     } else {
+//       alertBox("<strong>Oops!</strong> You chose a wrong answer, kindly go through the training again.", "danger")
+//       changeSlide(slidePosition)
+//     }
+// })
+
+$('.checkAnswer').on('click', function () {
+
+    let question_id = $('input[type="radio"]:checked', $(this).parent()).attr("name");
+    if (question_id == undefined) {
+        alertBox("<strong>Oops!</strong> You did not choose an answer, kindly choose an answer.", "danger");
+        return
+    }
+    let value = $('input[type="radio"]:checked', $(this).parent()).val();
+    // let slidePosition = parseInt(sessionStorage.getItem("slidePosition"));
+
+    sessionStorage.setItem("slidePosition", 0);
+
+    if (sessionStorage.getItem(question_id) == value) {
+        alertBox("<strong>Yay!</strong> You chose the correct answer.", "success");
+        $('.next', $(this).parent().parent()).removeClass('disabled');
+        // changeSlide(1);
+    } else {
+        alertBox("<strong>Oops!</strong> You chose a wrong answer, kindly go through the previous sections again.", "danger");
+        // changeSlide(slidePosition);
+        $('.next', $(this).parent().parent()).addClass('disabled');
+    }
+});
+
+function alertBox(message, _class) {
+    let alert = document.getElementById("alert");
+    alert.className = "alert alert-" + _class;
+    alert.innerHTML = message;
+    alert.style.display = "block";
+    setTimeout(function () {
+        alert.style.display = "none";
+    }, 3000);
+};
--- physionet-django/training/urls.py
+++ physionet-django/training/urls.py
@@ -4,4 +4,6 @@ from training import views
 
 urlpatterns = [
     path('training/', views.on_platform_training, name='op_training'),
+    path('settings/platform-training/<int:training_id>/', views.take_training, name='platform_training'),
+    path('settings/platform-training/', views.take_training, name='start_training')
 ]
