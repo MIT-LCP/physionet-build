ec44b37dfaed96117f8cd5e34d75dc77175b05aa
Amit Upreti <a.u.aua937@gmail.com>
2023-03-22 11:21:37 -0400

    Add Training Module
    Currently the training lacks proper organization. We have a OnPlatform Training which holds bunch of content and quizzes under it directly.
    
    On all online learning platforms or physical books, the training/courses are organized under chapter(in books) and modules/lessons and sections. With the idea of having modules under training instructors will be able to organize the training content under appropriate modules.
    
    Also, i didnot squash this commit just in case we want to revert the changes later.

--- physionet-django/training/models.py
+++ physionet-django/training/models.py
@@ -12,17 +12,34 @@ class OnPlatformTraining(models.Model):
         default_permissions = ('change',)
         unique_together = ('training_type', 'version')
 
+    def __str__(self):
+        return f'{self.training_type} v{self.version}'
+
+
+class Module(models.Model):
+    name = models.CharField(max_length=100)
+    training = models.ForeignKey('training.OnPlatformTraining',
+                                 on_delete=models.CASCADE, related_name='modules')
+    order = models.PositiveIntegerField()
+    description = SafeHTMLField()
+
+    class Meta:
+        unique_together = ('training', 'order')
+
+    def __str__(self):
+        return self.name
+
 
 class Quiz(models.Model):
     question = SafeHTMLField()
-    training = models.ForeignKey('training.OnPlatformTraining',
-                                 on_delete=models.CASCADE, related_name='quizzes')
+    module = models.ForeignKey('training.Module',
+                               on_delete=models.CASCADE, related_name='quizzes')
     order = models.PositiveIntegerField()
 
 
 class ContentBlock(models.Model):
-    training = models.ForeignKey('training.OnPlatformTraining',
-                                 on_delete=models.CASCADE, related_name='contents')
+    module = models.ForeignKey('training.Module',
+                               on_delete=models.CASCADE, related_name='contents')
     body = SafeHTMLField()
     order = models.PositiveIntegerField()
 
