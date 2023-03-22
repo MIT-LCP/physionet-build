57f4aa2b9d22eedddd655b2974c222013501ffa2
Amit Upreti <a.u.aua937@gmail.com>
2023-03-22 11:27:11 -0400

    fix admin.py to work with model update

--- physionet-django/training/admin.py
+++ physionet-django/training/admin.py
@@ -18,13 +18,25 @@ class QuizInline(admin.StackedInline):
     extra = 1
 
 
+class ModuleInline(admin.StackedInline):
+    model = models.Module
+    inlines = [ContentBlockInline, QuizInline]
+    extra = 1
+
+
 @admin.register(models.Quiz)
 class QuizAdmin(admin.ModelAdmin):
-    list_display = ('training', 'question', 'order')
+    list_display = ('module', 'question', 'order')
     inlines = [QuizChoiceInline, ]
+
+
+@admin.register(models.Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'training', 'order')
+    inlines = [ContentBlockInline, QuizInline]
 
 
 @admin.register(models.OnPlatformTraining)
 class OnPlatformTrainingAdmin(admin.ModelAdmin):
     list_display = ('training_type', 'version')
-    inlines = [ContentBlockInline, QuizInline]
+    inlines = [ModuleInline]
