60689c9d6d06a61347f393a76ac46ab4478b31f7
Amit Upreti <a.u.aua937@gmail.com>
2023-03-27 20:35:23 -0400

    clean take_training view
    Since all the code from here has been successfully
    refactored into take_module, it should be safe to clean this view.

--- physionet-django/training/views.py
+++ physionet-django/training/views.py
@@ -27,49 +27,8 @@ def take_training(request, training_id=None):
     if request.method == 'POST':
         if request.POST.get('training_type'):
             return redirect('platform_training', request.POST['training_type'])
-        # TODO remove the block below once the new view takes care of all aspects
-        op_training = OnPlatformTraining.objects.select_related('training_type').filter(
-            training_type__id=training_id).last()
 
-        # check if the questions are answered correctly
-        try:
-            user_question_answers = json.loads(request.POST['question_answers'])
-            # convert the keys to int(javascript sends them as string)
-            user_question_answers = {int(k): v for k, v in user_question_answers.items()}
-            for question in op_training.quizzes.all():
-                correct_answer = question.choices.filter(is_correct=True).first().id
-                if (question.id not in user_question_answers
-                        or user_question_answers.get(question.id) != correct_answer):
-                    messages.error(request, 'Please answer all questions correctly.')
-                    return redirect("edit_training")
-
-        except json.JSONDecodeError:
-            messages.error(request, 'Please submit the training correctly.')
-            return redirect("edit_training")
-
-        # save a training object to database in Training
-        training = Training()
-        slug = get_random_string(20)
-        while Training.objects.filter(slug=slug).exists():
-            slug = get_random_string(20)
-
-        training.slug = slug
-        training.training_type = op_training.training_type
-        training.user = request.user
-        training.process_datetime = timezone.now()
-        training.status = TrainingStatus.ACCEPTED
-        training.save()
-
-        training_questions = []
-        for question in op_training.training_type.questions.all():
-            training_questions.append(TrainingQuestion(training=training, question=question))
-
-        TrainingQuestion.objects.bulk_create(training_questions)
-
-        messages.success(
-            request, f'Congratulations! You completed the training {op_training.training_type.name} successfully.')
-
-        return redirect("edit_training",)
+        return redirect("edit_training")
 
     training = OnPlatformTraining.objects.prefetch_related(
         Prefetch("modules__quizzes", queryset=Quiz.objects.order_by("?")),
@@ -99,7 +58,6 @@ def take_module_training(request, training_id, module_id):
         return redirect('platform_training', training_id)
 
     if request.method == 'POST':
-
 
         # check if the questions are answered correctly
         try:
