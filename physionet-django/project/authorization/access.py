40ea1cf4298cafc0ce556b49504e7aa445c90663
Amit Upreti <a.u.aua937@gmail.com>
2023-03-31 11:44:14 -0400

    single method for authorization
    Here i created a single method for handling all authorization related stuff.
    Here the idea is that we simply authorization down to this method and all
    future changes can be done in this method.
    The current usage of authorization  is with `project.has_access` and `project.accessible_by`(HDN)  which we can also achieve with using this new method(if we want). for example
    
    >>> from project.authorization.access import get_accessible_projects
    >>> from user.models import User
    >>> user = User(username='amitupreti')
    >>> queryset = get_accessible_projects(user)
    >>> has_access = queryset.contains(project) #boolean result
    
    I will also add another method that copies the `project.has_access`
    in case that has a better performance.

---
+++ physionet-django/project/authorization/access.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+
+from django.db.models import Q
+
+from events.models import Event, EventDataset
+from project.models import AccessPolicy, DUASignature, DataAccessRequest, PublishedProject
+from user.models import Training, TrainingType
+
+
+def get_accessible_projects(user):
+    """
+    Returns query filter for published projects accessible by a specified user
+    """
+    query = Q(access_policy=AccessPolicy.OPEN) & Q(deprecated_files=False)
+
+    dua_signatures = DUASignature.objects.filter(user=user)
+
+    if user.is_authenticated:
+        query |= Q(access_policy=AccessPolicy.RESTRICTED) & Q(
+            duasignature__in=dua_signatures
+        )
+
+    if user.is_credentialed:
+        completed_training = (
+            Training.objects.get_valid()
+            .filter(user=user)
+            .values_list("training_type")
+        )
+        not_completed_training = TrainingType.objects.exclude(
+            pk__in=completed_training
+        )
+        required_training_complete = ~Q(
+            required_trainings__in=not_completed_training
+        )
+
+        accepted_data_access_requests = DataAccessRequest.objects.filter(
+            requester=user, status=DataAccessRequest.ACCEPT_REQUEST_VALUE
+        )
+        contributor_review_with_access = Q(
+            access_policy=AccessPolicy.CONTRIBUTOR_REVIEW
+        ) & Q(data_access_requests__in=accepted_data_access_requests)
+
+        credentialed_with_dua_signed = Q(
+            access_policy=AccessPolicy.CREDENTIALED
+        ) & Q(duasignature__in=dua_signatures)
+
+        query |= required_training_complete & (
+            contributor_review_with_access | credentialed_with_dua_signed
+        )
+
+    # add projects that are accessible through events
+    events_all = Event.objects.filter(Q(host=user) | Q(participants__user=user))
+    active_events = set(events_all.filter(end_date__gte=datetime.now()))
+    accessible_datasets = EventDataset.objects.filter(event__in=active_events, is_active=True)
+    accessible_projects_ids = []
+    for event_dataset in accessible_datasets:
+        if event_dataset.has_access(user):
+            accessible_projects_ids.append(event_dataset.dataset.id)
+    query |= Q(id__in=accessible_projects_ids)
+
+    return PublishedProject.objects.filter(query).distinct()
