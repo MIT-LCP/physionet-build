92556fbbe9f20b8710633ef60d5426e3103e8ab9
Amit Upreti <a.u.aua937@gmail.com>
2023-04-28 11:09:32 -0400

    refactor theme_generator
    the theme_generator is limited because of the hardcoded theme option, updating this will allow us to override the theme variables easily in future(so that we can use this to customize themes per deployment)

--- physionet-django/user/management/commands/compilestatic.py
+++ physionet-django/user/management/commands/compilestatic.py
@@ -5,15 +5,15 @@ from decouple import config
 from django.core.management import call_command
 
 
-def theme_generator(dark, primary):
+def theme_generator(themes_colors):
     # creating the theme file
     with open('static/bootstrap/scss/theme.scss', 'w') as f:
         # cleaning the current contents of the file
         f.truncate(0)
         # writing the new contents to the file
         f.write('$theme-colors: (\n')
-        f.write('"primary" : ' + primary + ',\n')
-        f.write('"dark" : ' + dark + ',\n')
+        for key, value in themes_colors.items():
+            f.write(f'"{key}" : {value},\n')
         f.write(');\n')
         f.write('@import "bootstrap"\n')
 
@@ -31,9 +31,12 @@ class Command(BaseCommand):
         primary = config('PRIMARY', default='#002A5C')
         if len(primary) != 7:
             raise CommandError('PRIMARY environment variable is not a valid hex color')
-
+        themes_colors = {
+            'dark': dark,
+            'primary': primary,
+        }
         # calling the theme generator function
-        theme_generator(dark, primary)
+        theme_generator(themes_colors)
         # calling the compile static command
         call_command('sass', 'static/bootstrap/scss/theme.scss', 'static/bootstrap/css/bootstrap.css')
 
