e036ff4f0b33e8451c73fc449b086a6588e8de56
Amit Upreti <a.u.aua937@gmail.com>
2023-05-11 18:46:10 -0400

    setup static-override folders for custom deployment
    As discussed on https://github.com/MIT-LCP/physionet-build/issues/1996,
    we are making the theme customizable for deployment
    by .env configuration. This lead us modifying existing css files
    adding configurable image files.
    This folder should be used to store deployment specific
    static files.
    
    It is expected that compilestatic will be ran first(which will
    generate the css, image files and store them to the static-override folder),
    and then collectstatic will be ran which will collect static files
    from static and static-override directories
    
    Note that, the `static-overrides` comes before the `static` on base.py
    This is intentional to make sure `static-overrides` is given
    first priority if there are same file in these two directories.

--- physionet-django/physionet/settings/base.py
+++ physionet-django/physionet/settings/base.py
@@ -174,7 +174,10 @@ MAX_ATTEMPTS = 5
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static-overrides'),
+    os.path.join(BASE_DIR, 'static'),
+]
 # Google Storge service account credentials
 if config('GOOGLE_APPLICATION_CREDENTIALS', default=None):
     GOOGLE_APPLICATION_CREDENTIALS = os.path.join(
