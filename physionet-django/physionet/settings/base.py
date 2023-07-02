b48b100a1846a0ac7f1aafbc4529791c3de72080
Rutvik Solanki <rutvikrj26@gmail.com>
2023-07-02 14:36:52 -0400

    Switched the order of Middleware.

--- physionet-django/physionet/settings/base.py
+++ physionet-django/physionet/settings/base.py
@@ -80,9 +80,9 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'oauth2_provider.middleware.OAuth2TokenMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'oauth2_provider.middleware.OAuth2TokenMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     # RedirectFallbackMiddleware should go at end of list, according
