8a22afafd5cdb423e7dd8b1d3e9ad1efd20a32f4
rutvikrj26 <rutvikrj26@gmail.com>
2023-07-24 21:53:42 -0400

    adding oauth test cases

--- physionet-django/oauth/tests.py
+++ physionet-django/oauth/tests.py
@@ -1,3 +1,56 @@
 from django.test import TestCase
+from user.models import User
+from oauth2_provider.models import get_application_model, AccessToken
+from django.utils import timezone
+from datetime import timedelta
 
-# Create your tests here.
+Application = get_application_model()
+
+class OAuthTestCase(TestCase):
+    def setUp(self):
+        self.test_user = User.objects.create_user("test_user", "test@user.com", "123456")
+        self.application = Application.objects.create(
+            name="Test Application",
+            redirect_uris="http://localhost:8000/oauth/hello/",
+            user=self.test_user,
+            client_type=Application.CLIENT_CONFIDENTIAL,
+            authorization_grant_type=Application.GRANT_AUTHORIZATION_CODE,
+        )
+
+def test_authorize(self):
+    # Set up test data
+    query_params = {
+        'client_id': self.application.client_id,
+        'response_type': 'code',
+        'state': 'random_state_string',
+        'redirect_uri': self.application.redirect_uris,
+    }
+
+    # Log in the test user
+    self.client.login(username='test_user', password='123456')
+
+    # Make a GET request to the authorize endpoint with the query parameters
+    response = self.client.get('/oauth/authorize/', data=query_params)
+
+    # Assert that the response status code is 200 (OK)
+    self.assertEqual(response.status_code, 200)
+
+def test_token(self):
+    # Set up test data
+    token = AccessToken.objects.create(
+        user=self.test_user,
+        token='1234567890',
+        application=self.application,
+        scope='read write',
+        expires=timezone.now() + timedelta(days=1)
+    )
+    auth_headers = {
+        'HTTP_AUTHORIZATION': 'Bearer ' + token.token,
+    }
+
+    # Make a GET request to the token endpoint with the authorization headers
+    response = self.client.get('/oauth/token/', **auth_headers)
+
+    # Assert that the response status code is 200 (OK)
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.json()['access_token'], token.token)
--- physionet-django/oauth/urls.py
+++ physionet-django/oauth/urls.py
@@ -8,13 +8,13 @@ oauth2_endpoint_views = [
     path('authorize/', oauth2_views.AuthorizationView.as_view(), name="authorize"),
     path('token/', oauth2_views.TokenView.as_view(), name="token"),
     path('revoke-token/', oauth2_views.RevokeTokenView.as_view(), name="revoke-token"),
+    path('applications/register/', oauth2_views.ApplicationRegistration.as_view(), name="register"),
 ]
 
 if settings.DEBUG:
     # OAuth2 Application Management endpoints
     oauth2_endpoint_views += [
         path('applications/', oauth2_views.ApplicationList.as_view(), name="list"),
-        path('applications/register/', oauth2_views.ApplicationRegistration.as_view(), name="register"),
         path('applications/<pk>/', oauth2_views.ApplicationDetail.as_view(), name="detail"),
         path('applications/<pk>/delete/', oauth2_views.ApplicationDelete.as_view(), name="delete"),
         path('applications/<pk>/update/', oauth2_views.ApplicationUpdate.as_view(), name="update"),
@@ -30,5 +30,5 @@ if settings.DEBUG:
 urlpatterns = [
     # OAuth 2 endpoints:
     path('', include((oauth2_endpoint_views, 'oauth2_provider'), namespace="oauth2_provider")),
-    path('hello', ApiEndpoint.as_view()),  # an example resource endpoint
+    path('hello', ApiEndpoint.as_view(), name='hello'),  # an example resource endpoint
 ]
