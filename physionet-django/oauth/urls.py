43ea40f9315ef08779e6338b1c84f097a7ce9692
rutvikrj26 <rutvikrj26@gmail.com>
2023-07-28 18:12:55 -0400

    Removed the Register endpoint from openly available APIs

--- physionet-django/oauth/urls.py
+++ physionet-django/oauth/urls.py
@@ -5,30 +5,58 @@ from .views import hello
 
 # OAuth2 provider endpoints
 oauth2_endpoint_views = [
-    path('authorize/', oauth2_views.AuthorizationView.as_view(), name="authorize"),
-    path('token/', oauth2_views.TokenView.as_view(), name="token"),
-    path('revoke-token/', oauth2_views.RevokeTokenView.as_view(), name="revoke-token"),
-    path('applications/register/', oauth2_views.ApplicationRegistration.as_view(), name="register"),
+    path("authorize/", oauth2_views.AuthorizationView.as_view(), name="authorize"),
+    path("token/", oauth2_views.TokenView.as_view(), name="token"),
+    path("revoke-token/", oauth2_views.RevokeTokenView.as_view(), name="revoke-token"),
 ]
 
 if settings.DEBUG:
     # OAuth2 Application Management endpoints
     oauth2_endpoint_views += [
-        path('applications/', oauth2_views.ApplicationList.as_view(), name="list"),
-        path('applications/<pk>/', oauth2_views.ApplicationDetail.as_view(), name="detail"),
-        path('applications/<pk>/delete/', oauth2_views.ApplicationDelete.as_view(), name="delete"),
-        path('applications/<pk>/update/', oauth2_views.ApplicationUpdate.as_view(), name="update"),
+        path("applications/", oauth2_views.ApplicationList.as_view(), name="list"),
+        path(
+            "applications/<pk>/",
+            oauth2_views.ApplicationDetail.as_view(),
+            name="detail",
+        ),
+        path(
+            "applications/<pk>/delete/",
+            oauth2_views.ApplicationDelete.as_view(),
+            name="delete",
+        ),
+        path(
+            "applications/<pk>/update/",
+            oauth2_views.ApplicationUpdate.as_view(),
+            name="update",
+        ),
+        path(
+            "applications/register/",
+            oauth2_views.ApplicationRegistration.as_view(),
+            name="register",
+        ),
     ]
 
     # OAuth2 Token Management endpoints
     oauth2_endpoint_views += [
-        path('authorized-tokens/', oauth2_views.AuthorizedTokensListView.as_view(), name="authorized-token-list"),
-        path('authorized-tokens/<pk>/delete/', oauth2_views.AuthorizedTokenDeleteView.as_view(),
-             name="authorized-token-delete"),
+        path(
+            "authorized-tokens/",
+            oauth2_views.AuthorizedTokensListView.as_view(),
+            name="authorized-token-list",
+        ),
+        path(
+            "authorized-tokens/<pk>/delete/",
+            oauth2_views.AuthorizedTokenDeleteView.as_view(),
+            name="authorized-token-delete",
+        ),
     ]
 
 urlpatterns = [
     # OAuth 2 endpoints:
-    path('', include((oauth2_endpoint_views, 'oauth2_provider'), namespace="oauth2_provider")),
-    path('hello', hello.as_view(), name='hello'),  # an example resource endpoint
+    path(
+        "",
+        include(
+            (oauth2_endpoint_views, "oauth2_provider"), namespace="oauth2_provider"
+        ),
+    ),
+    path("hello", hello.as_view(), name="hello"),  # an example resource endpoint
 ]
