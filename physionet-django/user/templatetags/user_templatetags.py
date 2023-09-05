14ef23879d62055d44638c9da6d50784d976ae5c
Rutvik Solanki <rutvikrj26@gmail.com>
2023-09-05 15:56:54 -0400

    Styling changes

--- physionet-django/user/templatetags/user_templatetags.py
+++ physionet-django/user/templatetags/user_templatetags.py
@@ -4,14 +4,24 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag('user/settings_tabs.html')
+@register.inclusion_tag("user/settings_tabs.html")
 def settings_tabs(hide_password_settings: bool):
-    default_tabs = ['Profile', 'Emails', 'Username', 'Cloud', 'ORCID', 'Credentialing', 'Course', 'Training', 'Agreements']
+    default_tabs = [
+        "Profile",
+        "Emails",
+        "Username",
+        "Cloud",
+        "ORCID",
+        "Credentialing",
+        "Course",
+        "Training",
+        "Agreements",
+    ]
     if not hide_password_settings:
-        default_tabs.insert(1, 'Password')
-    return {'settings_tabs': default_tabs}
+        default_tabs.insert(1, "Password")
+    return {"settings_tabs": default_tabs}
 
 
-@register.filter(name='has_group')
+@register.filter(name="has_group")
 def has_group(user, group_name):
     return user.groups.filter(name=group_name).exists()
