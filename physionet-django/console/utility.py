d54f2cbbd92c9c41d241185ddaea6397c34e9072
Benjamin Moody <benjaminmoody@gmail.com>
2023-09-14 13:45:45 -0400

    create_bucket: create the bucket in a single request.
    When creating a GCS bucket, we need to set various bucket properties
    in addition to the bucket name.  The
    google.cloud.storage.Client.create_bucket method allows setting some
    properties via keyword arguments.  However, the underlying HTTP API
    allows setting all of the properties at once - which means we can
    create the bucket and set its properties with a single request, rather
    than making two requests and risking that the second one fails.
    Note that the same pattern is used by physionet.gcs.create_bucket.
    
    (It might be better if we also could set the IAM policy at the same
    time - I'm not sure if this is possible or not - but on the other hand
    it might be better *not* to make the bucket accessible until after the
    project files have been uploaded.)

--- physionet-django/console/utility.py
+++ physionet-django/console/utility.py
@@ -53,11 +53,12 @@ def create_bucket(project, version, title, protected=True):
     """
     storage_client = storage.Client()
     bucket_name, email = bucket_info(project, version)
-    storage_client.create_bucket(bucket_name)
+
     bucket = storage_client.bucket(bucket_name)
     # Only bucket-level permissions are enforced; there are no per-file ACLs.
     bucket.iam_configuration.uniform_bucket_level_access_enabled = True
-    bucket.patch()
+    storage_client.create_bucket(bucket)
+
     LOGGER.info("Created bucket {0} for project {1}".format(
         bucket_name.lower(), project))
     if protected:
