b332cad899dcbdd4f7bdf6fda76adbeaf07aa774
rutvikrj26 <rutvikrj26@gmail.com>
2023-11-08 11:27:24 -0500

    implemented validate_version and simplified code for major version change

--- physionet-django/training/views.py
+++ physionet-django/training/views.py
@@ -1,3 +1,4 @@
+from hmac import new
 import json
 import operator
 from itertools import chain
@@ -10,6 +11,7 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 from django.utils.crypto import get_random_string
+from project.validators import validate_version
 
 from rest_framework.parsers import JSONParser
 
@@ -21,8 +23,26 @@ from training.models import CourseProgress, ModuleProgress, CompletedContent, Co
 from training.serializers import TrainingTypeSerializer
 
 
+# Utility Functions
+def is_major_change(version1, version2):
+    """
+    This function takes two version numbers as input parameters,
+    and returns True if the first digit of the version number is changed,
+    else returns False.
+    """
+    version1_first_digit = int(str(version1).split('.', maxsplit=1)[0])
+    version2_first_digit = int(str(version2).split('.', maxsplit=1)[0])
+    if version1_first_digit != version2_first_digit:
+        return True
+    return False
+
+
 @permission_required('training.change_course', raise_exception=True)
 def courses(request):
+    """
+    View function for managing courses.
+    Allows creation and updating of courses for a given training type.
+    """
     if request.POST:
 
         if request.POST.get('training_id') != "-1":
@@ -46,19 +66,20 @@ def courses(request):
         # Checking if the Training type with the same version already exists
         existing_course = Course.objects.filter(training_type=training_type)
         if existing_course.exists():
-            # checking if the version number is valid
-            if not all(map(lambda x: x.isdigit() or x == '.', str(file_data['courses'][0]['version']))):
+            existing_course_version = existing_course.order_by('-version').first().version
+            new_course_version = file_data['courses'][0]['version']
+            # checking if the new course file has a valid version
+            if not validate_version(file_data['courses'][0]['version']):
                 messages.error(request, 'Version number is not valid.')
             # checking if the version number is greater than the existing version
             elif float(file_data['courses'][0]['version']
                        ) <= float(existing_course.order_by('-version').first().version):
                 messages.error(request, 'Version number should be greater than the existing version.')
-            else:  # Checks passed and moving to saving the course
+            else:
                 serializer = TrainingTypeSerializer(training_type, data=file_data, partial=True)
                 if serializer.is_valid(raise_exception=False):
-                    # A Major Version change is detected : The First digit of the version number is changed
-                    if int(str(existing_course.order_by('-version').first().version).split('.')[0]) != int(str(
-                            file_data['courses'][0]['version']).split('.')[0]):
+                    if is_major_change(new_course_version,
+                                       existing_course_version):
                         # calling the update_course_for_major_version_change method to update the course
                         existing_course[0].update_course_for_major_version_change(training_type)
                     serializer.save()
@@ -85,6 +106,11 @@ def courses(request):
 
 @permission_required('training.change_course', raise_exception=True)
 def download_course(request, pk, version):
+    """
+    This view takes a primary key and a version number as input parameters,
+    and returns a JSON response containing information about the
+    training course with the specified primary key and version number.
+    """
     training_type = get_object_or_404(TrainingType, pk=pk)
     version = float(version)
     if training_type.required_field != RequiredField.PLATFORM:
