5c32785cb80196981e9383ec6c19cea965023a74
Benjamin Moody <benjaminmoody@gmail.com>
2024-10-01 14:03:05 -0400

    export.serializers: reformat for readability/editability.

--- physionet-django/export/serializers.py
+++ physionet-django/export/serializers.py
@@ -7,13 +7,17 @@ from project.models import PublishedProject, License, DUA
 class LicenseSerializer(serializers.ModelSerializer):
     class Meta:
         model = License
-        fields = ('name',)
+        fields = (
+            'name',
+        )
 
 
 class DUASerializer(serializers.ModelSerializer):
     class Meta:
         model = DUA
-        fields = ('name',)
+        fields = (
+            'name',
+        )
 
 
 class PublishedProjectSerializer(serializers.ModelSerializer):
@@ -22,19 +26,40 @@ class PublishedProjectSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = PublishedProject
-        fields = ('slug', 'title', 'abstract', 'license', 'dua', 'main_storage_size',
-                  'compressed_storage_size')
+        fields = (
+            'slug',
+            'title',
+            'abstract',
+            'license',
+            'dua',
+            'main_storage_size',
+            'compressed_storage_size',
+        )
 
 
 class ProjectVersionsSerializer(serializers.ModelSerializer):
     class Meta:
         model = PublishedProject
-        fields = ('slug', 'title', 'version', 'abstract')
+        fields = (
+            'slug',
+            'title',
+            'version',
+            'abstract',
+        )
 
 
 class PublishedProjectDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = PublishedProject
-        fields = ("title", "abstract", "version", "short_description",
-                  "project_home_page", "publish_datetime", "doi", "slug", "main_storage_size",
-                  "compressed_storage_size")
+        fields = (
+            'title',
+            'abstract',
+            'version',
+            'short_description',
+            'project_home_page',
+            'publish_datetime',
+            'doi',
+            'slug',
+            'main_storage_size',
+            'compressed_storage_size',
+        )
