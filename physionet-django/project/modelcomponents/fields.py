f1e945fcb731a86b51aaf43eea6aff68afec8c13
Benjamin Moody <benjaminmoody@gmail.com>
2025-01-03 16:46:04 -0500

    SafeHTMLField: replace CKEditor with TinyMCE.

--- physionet-django/project/modelcomponents/fields.py
+++ physionet-django/project/modelcomponents/fields.py
@@ -1,42 +1,39 @@
-import ckeditor.fields
-
 import bleach
+from django import forms
 from django.conf import settings
+from django.contrib.admin.widgets import AdminTextareaWidget
+from django.db import models
+from tinymce.widgets import AdminTinyMCE, TinyMCE
 
 from project.utility import LinkFilter
 
 
-class SafeHTMLField(ckeditor.fields.RichTextField):
+class SafeHTMLField(models.TextField):
     """
     An HTML text field that permits only "safe" content.
 
     On the client side, this field is displayed as an interactive
-    WYSIWYG editor (see ckeditor.fields.RichTextField.)
+    WYSIWYG editor (TinyMCE.)
 
     On the server side, the HTML text is "cleaned" using the bleach
     library to ensure that all tags are properly closed, entities are
     well-formed, etc., and to remove or escape any unsafe tags or
     attributes.
 
-    The permitted set of tags and attributes is generated from the
-    corresponding 'allowedContent' rules in settings.CKEDITOR_CONFIGS
-    (which also defines the client-side whitelisting rules and the set
-    of options that are visible to the user.)  For example:
+    The permitted sets of tags and attributes are defined by
+    settings.HTML_ALLOWED_CONTENT.  For example:
 
-        'allowedContent': {
-            'a': {'attributes': ['href']},
-            'em': True,
-            '*': {'attributes': ['title']},
+        HTML_ALLOWED_CONTENT = {
+            'default': {
+                'a': {'attributes': ['href']},
+                'em': True,
+                '*': {'attributes': ['title']},
+            },
         }
 
     This would permit the use of 'a' and 'em' tags (all other tags are
     forbidden.)  'a' tags are permitted to have an 'href' attribute,
     and any tag is permitted to have a 'title' attribute.
-
-    NOTE: This class does not use ckeditor's 'disallowedContent'
-    rules.  Those rules can be used to perform tag/attribute
-    blacklisting on the client side, but will not be enforced on the
-    server side.
     """
 
     # The following protocols may be used in 'href', 'src', and
@@ -56,12 +53,16 @@ class SafeHTMLField(ckeditor.fields.RichTextField):
 
     def __init__(self, config_name='default', strip=False,
                  strip_comments=True, **kwargs):
-        super().__init__(config_name=config_name, **kwargs)
+        super().__init__(**kwargs)
 
-        conf = settings.CKEDITOR_CONFIGS[config_name]
+        # Create a bleach.Cleaner for the allowed content, which is
+        # used to clean data (on the server side) when submitting the
+        # form.
+
+        allowed_content = settings.HTML_ALLOWED_CONTENT[config_name]
         tags = []
         attrs = {}
-        for (tag, props) in conf['allowedContent'].items():
+        for (tag, props) in allowed_content.items():
             if tag != '*':
                 tags.append(tag)
             if isinstance(props, dict) and 'attributes' in props:
@@ -75,6 +76,37 @@ class SafeHTMLField(ckeditor.fields.RichTextField):
                                        protocols=self._protocols,
                                        strip=strip,
                                        strip_comments=strip_comments)
+
+        # Create a corresponding filter expression (for cleaning data
+        # on the client side, e.g. when copying/pasting HTML):
+        # https://www.tiny.cloud/docs/tinymce/6/content-filtering/#valid_elements
+        # For example, "@[title|lang],a[href],em" permits only the 'a'
+        # and 'em' tags; 'a' tags are permitted to have an 'href'
+        # attribute, and any tag is permitted to have 'title' and/or
+        # 'lang' attributes.
+
+        valid_elements = []
+        if '*' in attrs:
+            valid_elements.append('@[' + '|'.join(attrs['*']) + ']')
+        for tag in tags:
+            tag_attrs = attrs.get(tag)
+            if tag_attrs:
+                expr = tag + '[' + '|'.join(tag_attrs) + ']'
+            else:
+                expr = tag
+            valid_elements.append(expr)
+
+        self._widget_mce_attrs = {
+            'valid_elements': ','.join(valid_elements)
+        }
+
+    def formfield(self, widget=None, **kwargs):
+        widget = widget or TinyMCE
+        if widget == AdminTextareaWidget:
+            widget = AdminTinyMCE
+        if isinstance(widget, type) and issubclass(widget, TinyMCE):
+            widget = widget(mce_attrs=self._widget_mce_attrs)
+        return super().formfield(widget=widget, **kwargs)
 
     def clean(self, value, model_instance):
         value = self._cleaner.clean(value)
