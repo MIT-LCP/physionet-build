d82660ec6b1c35f3d905e1fceac3b81256bec8ef
Tom Pollard <tpollard@mit.edu>
2025-04-07 16:42:56 -0400

    Update project serializer to include publish_date, doi, version, description. Update version serializer to include citation.

--- physionet-django/export/serializers.py
+++ physionet-django/export/serializers.py
@@ -23,11 +23,20 @@ class DUASerializer(serializers.ModelSerializer):
 class PublishedProjectSerializer(serializers.ModelSerializer):
     license = LicenseSerializer()
     dua = DUASerializer()
+    publish_date = serializers.SerializerMethodField()
+    core_doi = serializers.SerializerMethodField()
+    version_doi = serializers.SerializerMethodField()
 
     class Meta:
         model = PublishedProject
         fields = (
             'slug',
+            'version',
+            'core_doi',
+            'version_doi',
+            'is_latest_version',
+            'short_description',
+            'publish_date',
             'title',
             'abstract',
             'license',
@@ -36,8 +45,19 @@ class PublishedProjectSerializer(serializers.ModelSerializer):
             'compressed_storage_size',
         )
 
+    def get_publish_date(self, obj):
+        return obj.publish_datetime.date() if obj.publish_datetime else None
+
+    def get_core_doi(self, obj):
+        return obj.core_project.doi if hasattr(obj, 'core_project') else None
+
+    def get_version_doi(self, obj):
+        return obj.doi
+
 
 class ProjectVersionsSerializer(serializers.ModelSerializer):
+    citation = serializers.SerializerMethodField()
+
     class Meta:
         model = PublishedProject
         fields = (
@@ -45,7 +65,11 @@ class ProjectVersionsSerializer(serializers.ModelSerializer):
             'title',
             'version',
             'abstract',
+            'citation',
         )
+
+    def get_citation(self, obj):
+        return obj.citation_text(style="Vancouver")
 
 
 class PublishedProjectDetailSerializer(serializers.ModelSerializer):
