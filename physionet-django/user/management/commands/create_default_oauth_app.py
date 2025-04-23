6541f89fb3c5e5661acb762ec0516f54d6d54ac2
Tom Pollard <tpollard@mit.edu>
2025-04-23 12:02:46 -0400

    Create OAuth application for user-generated tokens during

--- physionet-django/user/management/commands/create_default_oauth_app.py
+++ physionet-django/user/management/commands/create_default_oauth_app.py
@@ -1,44 +1,64 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from oauth2_provider.models import get_application_model
+from django.contrib.auth import get_user_model
+
+
+def create_default_oauth_application(user=None, stdout=None):
+    app_name = getattr(settings, "OAUTH_CLIENT_APP_NAME", "PhysioNet Client")
+    Application = get_application_model()
+    User = get_user_model()
+
+    if not user:
+        user = User.objects.filter(is_superuser=True).first()
+        if not user:
+            msg = "No superuser found; skipping OAuth application creation."
+            if stdout:
+                stdout.write(msg)
+            else:
+                print(msg)
+            return
+
+    app, created = Application.objects.get_or_create(
+        name=app_name,
+        defaults={
+            'user': user,
+            'client_type': Application.CLIENT_CONFIDENTIAL,
+            'authorization_grant_type': Application.GRANT_PASSWORD,
+            'skip_authorization': True,
+        }
+    )
+
+    msg = (
+        f"Created OAuth application: {app.name}"
+        if created else
+        f'OAuth application "{app.name}" already exists.'
+    )
+    if stdout:
+        stdout.write(msg)
+    else:
+        print(msg)
 
 
 class Command(BaseCommand):
-    help = 'Creates the default OAuth application for the given superuser.'
+    help = 'Creates the default OAuth application.'
 
     def add_arguments(self, parser):
         parser.add_argument(
-            'username',
+            '--username',
             type=str,
-            help='The username of the superuser who will own the OAuth application.'
+            help='Optional superuser username to assign as the application owner.',
         )
 
     def handle(self, *args, **options):
-        Application = get_application_model()
         User = get_user_model()
+        username = options.get("username")
 
-        username = options['username']
-        try:
-            user = User.objects.get(username=username, is_superuser=True)
-        except User.DoesNotExist:
-            raise CommandError(f'Superuser with username "{username}" does not exist.')
+        user = None
+        if username:
+            try:
+                user = User.objects.get(username=username, is_superuser=True)
+            except User.DoesNotExist:
+                raise CommandError(f'Superuser with username "{username}" does not exist.')
 
-        app_name = getattr(settings, 'OAUTH_CLIENT_APP_NAME', 'Default OAuth Application')
-        client_type = Application.CLIENT_CONFIDENTIAL
-        authorization_grant_type = Application.GRANT_PASSWORD
-
-        app, created = Application.objects.get_or_create(
-            name=app_name,
-            defaults={
-                'user': user,
-                'client_type': client_type,
-                'authorization_grant_type': authorization_grant_type,
-                'skip_authorization': True,
-            }
-        )
-
-        if created:
-            self.stdout.write(self.style.SUCCESS(f'Created OAuth application: {app.name}'))
-        else:
-            self.stdout.write(self.style.WARNING(f'OAuth application "{app.name}" already exists.'))
+        create_default_oauth_application(user=user, stdout=self.stdout)
