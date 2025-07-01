14c845fec25d390920f6adf5f45199fd0ad81489
Tom Pollard <tpollard@mit.edu>
2025-07-01 13:06:13 -0400

    Remove forwarding header.

--- physionet-django/physionet/utility.py
+++ physionet-django/physionet/utility.py
@@ -363,11 +363,7 @@ def get_client_ip(request):
     """
     Get the client's IP address from the request.
     """
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
+    ip = request.META.get('REMOTE_ADDR')
     return ip
 
 
