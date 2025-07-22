644f87652a3a385208ecd46298917fe3a9070323
Tom Pollard <tpollard@mit.edu>
2025-07-22 17:02:42 -0400

    Truncate news content on front page.

--- physionet-django/physionet/templatetags/physionet_templatetags.py
+++ physionet-django/physionet/templatetags/physionet_templatetags.py
@@ -1,4 +1,7 @@
 from django import template
+from django.utils.html import strip_tags
+import re
+import html
 from physionet.models import StaticPage
 
 register = template.Library()
@@ -14,3 +17,31 @@ def get_static_page():
 def underscore(str_var):
     str_under = str_var.replace(' ', '_')
     return str_under
+
+
+@register.filter
+def news_preview(content, max_length=200):
+    """
+    Filter to create a preview of news content for the front page.
+    Removes images, strips HTML tags, and truncates.
+    """
+    if not content:
+        return ""
+
+    # Remove img tags and their content
+    content = re.sub(r'<img[^>]*>', '', content)
+
+    # Strip all HTML tags
+    text_content = strip_tags(content)
+
+    # Decode HTML entities
+    text_content = html.unescape(text_content)
+
+    # Remove extra whitespace
+    text_content = re.sub(r'\s+', ' ', text_content).strip()
+
+    # Truncate to max_length
+    if len(text_content) > max_length:
+        text_content = text_content[:max_length].rsplit(' ', 1)[0] + '...'
+
+    return text_content
