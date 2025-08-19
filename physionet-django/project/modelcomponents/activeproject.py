7750453f68c105cb7efa45f0d9c52af3fd910018
Benjamin Moody <benjaminmoody@gmail.com>
2025-08-19 16:08:56 -0400

    ActiveProject: make reopen_copyedit an atomic operation.
    When a project is reverted from NEEDS_APPROVAL to NEEDS_COPYEDIT
    state, other things should be done at the same time.  Keep the project
    state consistent by ensuring the ActiveProject, CopyeditLog, and
    Authors are all updated/created in a single transaction.

--- physionet-django/project/modelcomponents/activeproject.py
+++ physionet-django/project/modelcomponents/activeproject.py
@@ -444,11 +444,12 @@ class ActiveProject(Metadata, UnpublishedProject, SubmissionInfo):
         Reopen the project for copyediting
         """
         if self.submission_status == SubmissionStatus.NEEDS_APPROVAL:
-            self.submission_status = SubmissionStatus.NEEDS_COPYEDIT
-            self.copyedit_completion_datetime = None
-            self.save()
-            CopyeditLog.objects.create(project=self, is_reedit=True)
-            self.authors.all().update(approval_datetime=None)
+            with transaction.atomic():
+                self.submission_status = SubmissionStatus.NEEDS_COPYEDIT
+                self.copyedit_completion_datetime = None
+                self.save()
+                CopyeditLog.objects.create(project=self, is_reedit=True)
+                self.authors.all().update(approval_datetime=None)
 
     def approve_author(self, author):
         """"
