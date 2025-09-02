47846fcf32fb285f496b3ffb0100ccc09dbbda2c
Tom Pollard <tpollard@mit.edu>
2025-09-02 10:30:54 -0400

    Fix style

--- physionet-django/console/views.py
+++ physionet-django/console/views.py
@@ -228,7 +228,8 @@ def submitted_projects(request):
                    'copyedit_projects': copyedit_projects,
                    'approval_projects': approval_projects,
                    'publish_projects': publish_projects,
-                   'yesterday': yesterday})
+                   'yesterday': yesterday
+                   })
 
 
 @console_permission_required('project.change_activeproject')
@@ -270,7 +271,8 @@ def editor_home(request):
                    'copyedit_projects': copyedit_projects,
                    'approval_projects': approval_projects,
                    'publish_projects': publish_projects,
-                   'yesterday': yesterday, 'editor_home': True})
+                   'yesterday': yesterday, 'editor_home': True
+                   })
 
 
 def submission_info_redirect(request, project_slug):
@@ -387,7 +389,8 @@ def submission_info(request, project_slug):
 
                 LOGGER.info(f"Project root: {project_root}")
                 LOGGER.info(f"File path: {file_unpack_form.cleaned_data['file_path']}")
-                LOGGER.info(f"Full file path: {os.path.join(project_root, file_unpack_form.cleaned_data['file_path'])}")
+                full_path = os.path.join(project_root, file_unpack_form.cleaned_data['file_path'])
+                LOGGER.info(f"Full file path: {full_path}")
 
                 result = FileUnpacker.unpack_file(
                     project_root=project_root,
@@ -399,7 +402,8 @@ def submission_info(request, project_slug):
                 if result['success']:
                     messages.success(
                         request,
-                        f"Successfully unpacked {len(result['extracted_files'])} files to {result['extract_directory']}"
+                        f"Successfully unpacked {len(result['extracted_files'])} "
+                        f"files to {result['extract_directory']}"
                     )
                     # Reset form after successful unpacking
                     file_unpack_form = forms.FileUnpackForm()
@@ -417,21 +421,18 @@ def submission_info(request, project_slug):
 
     return render(request, 'console/submission_info.html',
                   {**submission_info_card_params(
-                   request,
-                   project,
-                   reassign_editor_form,
-                   embargo_form,
-                   internal_note_form,
-                   bulk_download=True,
-                   force_calculate=False,
-                   file_unpack_form=file_unpack_form
-                   ),
-                   'copyedit_logs': copyedit_logs,
-                   'passphrase': passphrase,
-                   'anonymous_url': anonymous_url,
-                   'file_unpack_form': file_unpack_form,
-                   }
-                  )
+                      request,
+                      project,
+                      reassign_editor_form,
+                      embargo_form,
+                      internal_note_form,
+                      bulk_download=True,
+                      force_calculate=False,
+                      file_unpack_form=file_unpack_form),
+                      'copyedit_logs': copyedit_logs,
+                      'passphrase': passphrase,
+                      'anonymous_url': anonymous_url,
+                      'file_unpack_form': file_unpack_form})
 
 
 @handling_editor
@@ -480,22 +481,18 @@ def edit_submission(request, project_slug, *args, **kwargs):
         edit_submission_form = forms.EditSubmissionForm(
             resource_type=project.resource_type, instance=edit_log)
 
-    return render(request,
-                  'console/edit_submission.html',
+    return render(request, 'console/edit_submission.html',
                   {**submission_info_card_params(
-                   request,
-                   project,
-                   reassign_editor_form,
-                   embargo_form,
-                   internal_note_form,
-                   bulk_download=True,
-                   force_calculate=False,
-                   file_unpack_form=None
-                   ),
-                   'edit_submission_form': edit_submission_form,
-                   'editor_home': True,
-                   }
-                  )
+                      request,
+                      project,
+                      reassign_editor_form,
+                      embargo_form,
+                      internal_note_form,
+                      bulk_download=True,
+                      force_calculate=False,
+                      file_unpack_form=None),
+                      'edit_submission_form': edit_submission_form,
+                      'editor_home': True})
 
 
 @handling_editor
@@ -628,47 +625,42 @@ def copyedit_submission(request, project_slug, *args, **kwargs):
 
     edit_url = reverse('edit_content_item', args=[project.slug])
 
-    response = render(
-        request,
-        'console/copyedit_submission.html',
-            {**submission_info_card_params(
-                   request,
-                   project,
-                   reassign_editor_form,
-                   embargo_form,
-                   internal_note_form,
-                   bulk_download=False,
-                   force_calculate=True,
-                   file_unpack_form=None
-                   ),
-         'description_form': description_form,
-         'ethics_form': ethics_form,
-         'individual_size_limit': readable_size(ActiveProject.INDIVIDUAL_FILE_SIZE_LIMIT),
-         'access_form': access_form,
-         'reference_formset': reference_formset,
-         'publication_formset': publication_formset,
-         'topic_formset': topic_formset,
-         'storage_type': settings.STORAGE_TYPE,
-         'upload_files_form': upload_files_form,
-         'create_folder_form': create_folder_form,
-         'rename_item_form': rename_item_form,
-         'move_items_form': move_items_form,
-         'delete_items_form': delete_items_form,
-         'subdir': subdir,
-         'display_files': display_files,
-         'display_dirs': display_dirs,
-         'dir_breadcrumbs': dir_breadcrumbs,
-         'file_error': file_error,
-         'editor_home': True,
-         'is_editor': True,
-         'files_editable': True,
-         'copyedit_form': copyedit_form,
-         'copyedit_logs': copyedit_logs,
-         'add_item_url': edit_url,
-         'remove_item_url': edit_url,
-         'discovery_form': discovery_form,
-         },
-    )
+    response = render(request, 'console/copyedit_submission.html',
+                      {**submission_info_card_params(request,
+                                                     project,
+                                                     reassign_editor_form,
+                                                     embargo_form,
+                                                     internal_note_form,
+                                                     bulk_download=False,
+                                                     force_calculate=True,
+                                                     file_unpack_form=None),
+                       'description_form': description_form,
+                       'ethics_form': ethics_form,
+                       'individual_size_limit': readable_size(ActiveProject.INDIVIDUAL_FILE_SIZE_LIMIT),
+                       'access_form': access_form,
+                       'reference_formset': reference_formset,
+                       'publication_formset': publication_formset,
+                       'topic_formset': topic_formset,
+                       'storage_type': settings.STORAGE_TYPE,
+                       'upload_files_form': upload_files_form,
+                       'create_folder_form': create_folder_form,
+                       'rename_item_form': rename_item_form,
+                       'move_items_form': move_items_form,
+                       'delete_items_form': delete_items_form,
+                       'subdir': subdir,
+                       'display_files': display_files,
+                       'display_dirs': display_dirs,
+                       'dir_breadcrumbs': dir_breadcrumbs,
+                       'file_error': file_error,
+                       'editor_home': True,
+                       'is_editor': True,
+                       'files_editable': True,
+                       'copyedit_form': copyedit_form,
+                       'copyedit_logs': copyedit_logs,
+                       'add_item_url': edit_url,
+                       'remove_item_url': edit_url,
+                       'discovery_form': discovery_form})
+
     if description_form_saved:
         set_saved_fields_cookie(description_form, request.path, response)
     return response
@@ -706,23 +698,21 @@ def awaiting_authors(request, project_slug, *args, **kwargs):
 
     yesterday = timezone.now() + timezone.timedelta(days=-1)
 
-    return render(request,
-                  'console/awaiting_authors.html',
+    return render(request, 'console/awaiting_authors.html',
                   {**submission_info_card_params(
-                   request,
-                   project,
-                   reassign_editor_form,
-                   embargo_form,
-                   internal_note_form,
-                   bulk_download=True,
-                   force_calculate=False,
-                   file_unpack_form=None
-                   ),
-                   'copyedit_logs': copyedit_logs,
-                   'outstanding_emails': outstanding_emails,
-                   'yesterday': yesterday,
-                   'editor_home': True,
-                   'reassign_editor_form': reassign_editor_form})
+                      request,
+                      project,
+                      reassign_editor_form,
+                      embargo_form,
+                      internal_note_form,
+                      bulk_download=True,
+                      force_calculate=False,
+                      file_unpack_form=None),
+                      'copyedit_logs': copyedit_logs,
+                      'outstanding_emails': outstanding_emails,
+                      'yesterday': yesterday,
+                      'editor_home': True,
+                      'reassign_editor_form': reassign_editor_form})
 
 
 @handling_editor
@@ -824,25 +814,21 @@ def publish_submission(request, project_slug, *args, **kwargs):
     publishable = project.is_publishable()
     publish_form = forms.PublishForm(project=project)
 
-    return render(request,
-                  'console/publish_submission.html',
+    return render(request, 'console/publish_submission.html',
                   {**submission_info_card_params(
-                   request,
-                   project,
-                   reassign_editor_form,
-                   embargo_form,
-                   internal_note_form,
-                   bulk_download=True,
-                   force_calculate=True,
-                   file_unpack_form=None
-                   ),
-                   'publishable': publishable,
-                   'copyedit_logs': copyedit_logs,
-                   'publish_form': publish_form,
-                   'max_slug_length': MAX_PROJECT_SLUG_LENGTH,
-                   'editor_home': True,
-                   }
-                  )
+                      request,
+                      project,
+                      reassign_editor_form,
+                      embargo_form,
+                      internal_note_form,
+                      bulk_download=True,
+                      force_calculate=True,
+                      file_unpack_form=None),
+                      'publishable': publishable,
+                      'copyedit_logs': copyedit_logs,
+                      'publish_form': publish_form,
+                      'max_slug_length': MAX_PROJECT_SLUG_LENGTH,
+                      'editor_home': True})
 
 
 @console_permission_required('project.change_storagerequest')
