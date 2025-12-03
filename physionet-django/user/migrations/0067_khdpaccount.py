70c0e4ce31fc5c0e6b00595b69fa4a528b6191b5
chrystinne <chrystinne@gmail.com>
2025-12-03 17:15:19 -0500

    Add migration for KhdpAccount model

---
+++ physionet-django/user/migrations/0067_khdpaccount.py
@@ -0,0 +1,20 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('user', '0066_previous_migration'),
+    ]
+    operations = [
+        migrations.CreateModel(
+            name='KhdpAccount',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('khdp_id', models.CharField(max_length=128, unique=True)),
+                ('khdp_username', models.CharField(max_length=128)),
+                ('khdp_email', models.EmailField()),
+                ('linked_at', models.DateTimeField(auto_now_add=True)),
+                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='khdp_account', to='user.User')),
+            ],
+        ),
+    ]
