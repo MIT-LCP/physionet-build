df1b99a621e90cd57d62ad131d160fa7010cc3b5
Tom Pollard <tpollard@mit.edu>
2026-02-23 14:33:07 -0500

    Add in-app notification model.

--- physionet-django/notification/admin.py
+++ physionet-django/notification/admin.py
@@ -4,3 +4,11 @@ from notification import models
 
 
 admin.site.register(models.News)
+
+
+@admin.register(models.Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('recipient', 'notification_type', 'message', 'is_read', 'created_datetime')
+    list_filter = ('is_read', 'notification_type', 'created_datetime')
+    search_fields = ('recipient__username', 'recipient__email', 'message')
+    raw_id_fields = ('recipient', 'actor')
