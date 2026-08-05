6d97e21d8daeddeb998108f0b332c377d6e464ea
Benjamin Moody <benjaminmoody@gmail.com>
2026-08-05 14:41:52 -0400

    test-upgrade.sh: consider the Python version for caching purposes.
    test-upgrade.sh caches the results of "pip3 install" for speed.
    
    The precise set of packages and the layout of the installation (e.g.,
    the location of the site-packages directory) will depend on the Python
    interpreter being used.  Thus, we should not reuse a cached
    installation that was created by a different ABI-incompatible
    interpreter.  As a rough attempt at avoiding this, ensure that the
    cache file names include the implementation name and the major/minor
    Python version.

--- test-upgrade.sh
+++ test-upgrade.sh
@@ -107,6 +107,9 @@ oldrev=$(git rev-parse --verify "$oldrevname^{commit}")
 currevname=$(git describe --all --always --dirty)
 
 PYTHON=${PYTHON:-python3}
+py_ver=$("$PYTHON" -c 'import sys; print("%s-%s.%s" % (
+    sys.implementation.name, *sys.version_info[0:2]
+))')
 
 ################################################################
 # Functions for reporting test results
@@ -241,7 +244,7 @@ export PATH=$venvdir/bin:$PATH
     cd "$olddir/physionet-django"
 
     msg_testing "Installing old requirements"
-    cachefile=$venvcachedir/$old_reqs_hash.tar.gz
+    cachefile=$venvcachedir/$old_reqs_hash-$py_ver.tar.gz
     if [ -n "$venvcachedir" ] && [ -f "$cachefile" ]; then
         prereq_cmd mkdir "$venvdir"
         prereq_cmd tar -xzf "$cachefile" -C "$venvdir"
@@ -268,7 +271,7 @@ export PATH=$venvdir/bin:$PATH
 
     msg_testing "Installing new requirements"
     if ! cmp -s "$olddir/requirements.txt" "$topdir/requirements.txt"; then
-        cachefile=$venvcachedir/$old_reqs_hash-$new_reqs_hash.tar.gz
+        cachefile=$venvcachedir/$old_reqs_hash-$new_reqs_hash-$py_ver.tar.gz
         if [ -n "$venvcachedir" ] && [ -f "$cachefile" ]; then
             prereq_cmd rm -rf "$venvdir"
             prereq_cmd mkdir "$venvdir"
