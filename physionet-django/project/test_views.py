ac8ee2d1caa79da491fab1750fdb57cdc6fe9d0b
chrystinne <chrystinne@gmail.com>
2026-09-03 11:31:10 -0400

    Test pending training with unmet requirements.

--- physionet-django/project/test_views.py
+++ physionet-django/project/test_views.py
@@ -36,7 +36,8 @@ from project.models import (
     SubmissionStatus,
     AWS
 )
-from user.models import User
+from user.enums import TrainingStatus
+from user.models import Training, TrainingType, User
 from user.test_views import TestMixin, prevent_request_warnings
 
 PROJECT_VIEWS = [
@@ -707,6 +708,27 @@ class TestAccessPublished(TestMixin):
     Published projects.
 
     """
+    def test_pending_training_does_not_hide_other_requirements(self):
+        project = PublishedProject.objects.get(title='Demo eICU Collaborative Research Database')
+        user = User.objects.get(email='rgmark@mit.edu')
+        reviewed_training = TrainingType.objects.create(name='Reviewed training')
+        outstanding_training = TrainingType.objects.create(name='Outstanding training')
+        project.required_trainings.set([reviewed_training, outstanding_training])
+        Training.objects.create(
+            user=user,
+            slug='reviewed-training',
+            training_type=reviewed_training,
+            status=TrainingStatus.REVIEW,
+            reviewer_comments='',
+        )
+        self.client.login(username=user.username, password='Tester11!')
+
+        response = self.client.get(reverse('published_project', args=(project.slug, project.version)))
+
+        self.assertContains(response, reviewed_training.name)
+        self.assertContains(response, outstanding_training.name)
+        self.assertNotContains(response, 'Have your submitted training report approved.')
+
     @prevent_request_warnings
     def test_credentialed(self):
         """
