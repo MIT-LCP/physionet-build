0350b5eb240947d04b7aa956af9855adbf6e89cf
chrystinne <chrystinne@gmail.com>
2026-09-03 11:30:41 -0400

    Show pending message only when all trainings are under review.

--- physionet-django/project/views.py
+++ physionet-django/project/views.py
@@ -1998,15 +1998,20 @@ def published_project(request, project_slug, version, subdir=''):
         else Training.objects.get_valid().filter(training_type__in=project.required_trainings.all(), user=user).count()
         == project.required_trainings.count()
     )
-    has_training_under_review = (
-        False
-        if not user.is_authenticated
-        else Training.objects.filter(
+    has_training_under_review = False
+    if user.is_authenticated:
+        valid_training_type_ids = Training.objects.get_valid().filter(
+            training_type__in=project.required_trainings.all(), user=user
+        ).values('training_type_id')
+        missing_training_types = project.required_trainings.exclude(id__in=valid_training_type_ids)
+        under_review_training_type_ids = Training.objects.filter(
             status=TrainingStatus.REVIEW,
-            training_type__in=project.required_trainings.all(),
+            training_type__in=missing_training_types,
             user=user,
+        ).values('training_type_id')
+        has_training_under_review = missing_training_types.exists() and not missing_training_types.exclude(
+            id__in=under_review_training_type_ids
         ).exists()
-    )
     current_site = get_current_site(request)
     bulk_url_prefix = notification.get_url_prefix(request, bulk_download=True)
     all_project_versions = PublishedProject.objects.filter(slug=project_slug).order_by('version_order')
